b716e364084fcc992d73edde47aa97969fca7469
Benito van der Zander <benito@benibela.de>
2009-01-16 16:52:21 +0000

    modified install instructions
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@11 92cce386-e96d-4106-809f-596c673a3fd8

--- BUILD.sh
+++ BUILD.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "Texmaker compilation :"
-echo "(make sure qcodeedit is installed)"
+echo "TexMakerX compilation :"
 echo "----------------------------------------"
 echo "Enter path to QT4 (/usr/lib/qt4 or ...):"
 read QTDIR
@@ -14,19 +13,19 @@ if [ "$SYSTEM" = 1 ]
 then
 echo "Enter PREFIX (/usr , /usr/local or /opt) :"
 read PREFIX
-qmake -unix PREFIX=$PREFIX texmaker.pro
-make
+qmake -unix PREFIX=$PREFIX texmakerx.pro
+make release
 make install
 echo "Compilation and installation done"
-echo "Icons and desktop file can be found in the $PREFIX/share/texmaker directory"
+echo "Icons and desktop file can be found in the $PREFIX/share/texmakerx directory"
 # set the -spec option, if necessary. Ex : qmake -unix -spec linux-g++ PREFIX=$PREFIX texmaker.pro
 exit 0
 fi
 
 if [ "$SYSTEM" = 2 ] 
 then
-qmake -macx -spec macx-g++ texmaker.pro
-make
+qmake -macx -spec macx-g++ texmakerx.pro
+make release
 make install
 echo "Compilation and installation done"
 exit 0
