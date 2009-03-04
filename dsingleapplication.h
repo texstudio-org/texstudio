cecb93da64a63a57d65460664a1d318f81566693
benibela <benito@benibela.de>
2009-03-04 15:01:23 +0000

    fixed crash on nero KDE (thread mistake in single application)
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@73 92cce386-e96d-4106-809f-596c673a3fd8

--- dsingleapplication.h
+++ dsingleapplication.h
@@ -106,7 +106,7 @@ public:
   PortStatus status() const;
   void check( int port );
   QTcpSocket* transferSocketOwnership();
-
+  void endChecks(); //important, this deletes the socket
 protected:
   void run();
 
