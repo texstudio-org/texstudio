7fa9cbd913baecfc304aef225bc79db917336c7f
benibela <benito@benibela.de>
2009-04-02 22:15:26 +0000

    automatically MiKTeX detection on Windows
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@109 92cce386-e96d-4106-809f-596c673a3fd8

--- buildmanager.cpp
+++ buildmanager.cpp
@@ -3,6 +3,7 @@
 #include <QCoreApplication>
 #include <QDir>
 #include <QMessageBox>
+#include <QProcess>
 
 #ifdef Q_WS_WIN
 #include "windows.h"
@@ -41,6 +42,7 @@ QString BuildManager::parseExtendedCommandLine(QString str, const QFileInfo &mai
                     if (str.at(i)==QChar(')')) { endMode=1;break;}
                     else if (str.at(i)==QChar(' ')||str.at(i)==QChar('\t')) { endMode=2;break;}
                     else if (str.at(i)==QChar('\"')) { endMode=3;break;}
+                    else if (str.at(i)==QChar('.')) { endMode=4;break;}
                     command+=str.at(i);
                     i++;
                 }
@@ -59,6 +61,7 @@ QString BuildManager::parseExtendedCommandLine(QString str, const QFileInfo &mai
                 switch (endMode) {
                     case 2: command+=" ";  break;
                     case 3: command="\""+command+"\""; break;
+                    case 4: command+="."; break;
                     default:;
                 }
                 result+=command;
@@ -77,6 +80,62 @@ int BuildManager::x11desktop_env(){
     if (!kdesession.isEmpty()) return 3;
     return 0;
 }
+
+#ifdef Q_WS_WIN
+typedef BOOL (* AssocQueryStringAFunc) (DWORD, DWORD, char*, char*, char*, DWORD*);
+QString W32_FileAssociation(QString ext){
+    if (ext=="") return "";
+    if (ext[0]!=QChar('.')) ext='.'+ext;
+    QString result="";
+    QByteArray ba=ext.toLocal8Bit();
+    HMODULE mod = LoadLibraryA("shlwapi.dll");
+    AssocQueryStringAFunc assoc=(AssocQueryStringAFunc)(GetProcAddress(mod,"AssocQueryStringA"));
+    if (assoc) {
+        const DWORD ASSOCSTR_COMMAND = 1;
+        char buf[1024];
+        DWORD buflen=1023;
+        if (assoc(0, ASSOCSTR_COMMAND, ba.data(), "open", &buf[0], &buflen)==S_OK) {
+            buf[buflen]=0;
+            result=QString::fromAscii(buf);
+            result.replace("%1","?am"+ext);
+            //QMessageBox::information(0,result,result,0);
+        };
+    }
+    FreeLibrary(mod);
+    return result;
+}
+QString findFileInPath(QString fileName){
+    QStringList env= QProcess::systemEnvironment();    //QMessageBox::information(0,env.join("  \n"),env.join("  \n"),0);
+    int i=env.indexOf(QRegExp("^PATH=.*", Qt::CaseInsensitive));
+    if (i==-1) return "";
+    QString path=env[i].mid(5); //skip path=
+    QStringList paths=path.split(";"); //windows
+    foreach (QString p, paths) 
+        if (p.endsWith("/") && QFileInfo(p+fileName).exists()) return (p+fileName);
+        else if (p.endsWith("\\") && QFileInfo(p+fileName).exists()) return (p+fileName);
+        else if (QFileInfo(p+"\\"+fileName).exists()) return (p+"\\"+fileName);
+    return "";
+}
+QString getMiKTeXBinPath(){
+    QSettings reg("HKEY_CURRENT_USER\\Software", QSettings::NativeFormat);
+    QString mikPath = reg.value("MiK/MikTeX/CurrentVersion/MiKTeX/Install Root", "").toString();
+    if (mikPath.isEmpty()) return "";
+    if (!mikPath.endsWith("\\") && !mikPath.endsWith("/")) 
+        mikPath+="\\"; //windows
+    return mikPath+"miktex\\bin\\";
+}
+QString searchBaseCommand(QString cmd, QString options, bool mustExist=false){
+    QString fileName=cmd+".exe";
+    if (findFileInPath(fileName).isEmpty()) {
+        QString mikPath=getMiKTeXBinPath();
+        QMessageBox::information(0,mikPath,mikPath,0);
+        if (!mikPath.isEmpty() && QFileInfo(mikPath+fileName).exists())
+            return mikPath+fileName+options;
+    } else return cmd+options;
+    if (mustExist) return "";
+    return cmd+options;
+}
+#endif
 
 QString BuildManager::guessCommandName(LatexCommand cmd){
     #ifdef Q_WS_MACX
@@ -100,32 +159,53 @@ QString BuildManager::guessCommandName(LatexCommand cmd){
     #endif
     #ifdef Q_WS_WIN
     switch (cmd) {
-        case CMD_LATEX: return "latex -interaction=nonstopmode %.tex"; 
-        case CMD_DVIPS: return "dvips -o %.ps %.dvi"; 
-        case CMD_PS2PDF: return "ps2pdf %.ps"; 
-        case CMD_MAKEINDEX: return "makeindex %.idx"; 
-        case CMD_BIBTEX: return "bibtex %"; 
-        case CMD_PDFLATEX: return "pdflatex -interaction=nonstopmode %.tex"; 
-        case CMD_DVIPDF: return "dvipdfm %.dvi"; 
-        case CMD_METAPOST: return "mpost --interaction nonstopmode "; 
-        case CMD_VIEWDVI: 
-            if (QFileInfo("C:/Program Files/MiKTeX 2.7/miktex/bin/yap.exe").exists()) 
-                return "\"C:/Program Files/MiKTeX 2.7/miktex/bin/yap.exe\" -1 -s @?ame \"?am).dvi\"";
+        case CMD_LATEX: return searchBaseCommand("latex"," -interaction=nonstopmode %.tex");
+        case CMD_DVIPS: return searchBaseCommand("dvips"," -o %.ps %.dvi"); 
+        case CMD_PS2PDF: return searchBaseCommand("ps2pdf"," %.ps");
+        case CMD_MAKEINDEX: return searchBaseCommand("makeindex"," %.idx"); 
+        case CMD_BIBTEX: return searchBaseCommand("bibtex"," %"); 
+        case CMD_PDFLATEX: return searchBaseCommand("pdflatex"," -interaction=nonstopmode %.tex"); 
+        case CMD_DVIPDF:   return searchBaseCommand("dvipdfm"," %.dvi"); 
+        case CMD_METAPOST:return searchBaseCommand("mpost"," --interaction nonstopmode "); 
+        case CMD_VIEWDVI: {
+            const QString yapOptions = " -1 -s @?ame \"?am.dvi\"";
+            QString def=W32_FileAssociation(".dvi");
+            if (!def.isEmpty()) {
+                if (def.contains("yap.exe")) {
+                    def=def.trimmed();
+                    if (def.endsWith("\"?am.dvi\"")) def.replace("\"?am.dvi\"",yapOptions);
+                    else if (def.endsWith("?am.dvi")) def.replace("?am.dvi",yapOptions);
+                }
+                return def;
+            }
+            def=searchBaseCommand("yap",yapOptions,true);
+            if (!def.isEmpty()) return def;
+            else if (QFileInfo("C:/Program Files/MiKTeX 2.7/miktex/bin/yap.exe").exists()) 
+                return "\"C:/Program Files/MiKTeX 2.7/miktex/bin/yap.exe\" " + yapOptions;
             else if (QFileInfo("C:/Program Files/MiKTeX 2.5/miktex/bin/yap.exe").exists()) 
-                return "\"C:/Program Files/MiKTeX 2.5/miktex/bin/yap.exe\" -1 -s @?ame \"?am).dvi\"";
+                return "\"C:/Program Files/MiKTeX 2.5/miktex/bin/yap.exe\" " + yapOptions;
             else if (QFileInfo("C:/texmf/miktex/bin/yap.exe").exists()) 
-                return "C:/texmf/miktex/bin/yap.exe -1 -s @?ame \"?am).dvi\"";
+                return "C:/texmf/miktex/bin/yap.exe " + yapOptions;
             else break;
-        case CMD_VIEWPS: 
-            if (QFileInfo("\"C:/Program Files/Ghostgum/gsview/gsview32.exe\"").exists()) 
-                return "\"C:/Program Files/Ghostgum/gsview/gsview32.exe\" \"?am).ps\""; 
+        }
+        case CMD_VIEWPS: {
+            QString def=W32_FileAssociation(".ps");
+            if (!def.isEmpty()) 
+                return def;
+            else if (QFileInfo("\"C:/Program Files/Ghostgum/gsview/gsview32.exe\"").exists()) 
+                return "\"C:/Program Files/Ghostgum/gsview/gsview32.exe\" \"?am.ps\""; 
             else break;
-        case CMD_VIEWPDF: 
-            if (QFileInfo("C:/Program Files/Adobe/Reader 9.0/Reader/AcroRd32.exe").exists()) 
-                return "\"C:/Program Files/Adobe/Reader 9.0/Reader/AcroRd32.exe\" \"?am).pdf\"";
+        }
+        case CMD_VIEWPDF: {
+            QString def=W32_FileAssociation(".pdf");
+            if (!def.isEmpty()) 
+                return def;
+            else if (QFileInfo("C:/Program Files/Adobe/Reader 9.0/Reader/AcroRd32.exe").exists()) 
+                return "\"C:/Program Files/Adobe/Reader 9.0/Reader/AcroRd32.exe\" \"?am.pdf\"";
             else if (QFileInfo("C:/Program Files/Adobe/Reader 8.0/Reader/AcroRd32.exe").exists()) 
-                return "\"C:/Program Files/Adobe/Reader 8.0/Reader/AcroRd32.exe\" \"?am).pdf\"";
+                return "\"C:/Program Files/Adobe/Reader 8.0/Reader/AcroRd32.exe\" \"?am.pdf\"";
             else break;
+        }
         case CMD_GHOSTSCRIPT: 
             if (QFileInfo("C:/Program Files/gs/gs8.63/bin/gswin32c.exe").exists()) 
                 return "\"C:/Program Files/gs/gs8.63/bin/gswin32c.exe\"";
@@ -217,7 +297,7 @@ bool BuildManager::executeDDE(QString ddePseudoURL){
     foreach (QString s, commands) {
         QString temp=("["+s.trimmed());
         QByteArray ba= temp.toLocal8Bit();
-        HDDEDATA req= DdeCreateDataHandle(pidInst, (LPBYTE) ba.data(), ba.size()+1, 0, 0, CF_TEXT, NULL);
+        HDDEDATA req= DdeCreateDataHandle(pidInst, (LPBYTE) ba.data(), ba.size()+1, 0, 0, CF_TEXT, 0);
         if (req) {
             HDDEDATA recData=DdeClientTransaction((BYTE*)req, (DWORD)-1, hConv, 0, 0, XTYP_EXECUTE, 1000, 0);
             DdeFreeDataHandle(req);
