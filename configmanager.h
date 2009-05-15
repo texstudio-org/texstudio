ee3b04f084b2b076148ed9cd5f9fbc6406c59d60
Benito van der Zander <benito@benibela.de>
2009-05-15 19:43:08 +0000

    moved config around (shortcuts, recent list), added customizable recent list lengths
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@195 92cce386-e96d-4106-809f-596c673a3fd8

--- configmanager.h
+++ configmanager.h
@@ -2,8 +2,11 @@
 #define CONFIGMANAGER_H
 
 #include "configdialog.h"
+#include <QDomElement>
+#include <QMenuBar>
 #include <QSettings>
 #include <QStyle>
+#include <QTreeWidgetItem>
 
 class ConfigManager: public QObject {
 	Q_OBJECT
@@ -31,6 +34,36 @@ public:
 	int interfaceFontSize;
 	
 	QFont editorFont;
+
+	QString lastDocument; //last opened document, for file open directory
+	QStringList recentFilesList, recentProjectList;
+	int maxRecentFiles, maxRecentProjects;
+	QStringList sessionFilesToRestore;
+	QString sessionMaster;
+	QString sessionCurrent;
+
+	bool addRecentFile(const QString & fileName, bool asMaster); //adds a recent file
+	void updateRecentFiles(bool alwaysRecreateMenuItems=false);
+	
+	//menus
+	QObject* menuParent; //lets assume there is only one
+	QMenuBar* menuParentsBar; //that's ugly, but faster as long as there is only one (both values could be extracted from the action's name)
+	QList<QMenu*> managedMenus;
+	QHash<QString,QKeySequence> managedMenuShortcuts;
+	QList<QPair<QString,QString> > managedMenuNewShortcuts;
+	
+	QMenu* newManagedMenu(const QString &id,const QString &text);
+	QMenu* newManagedMenu(QMenu* menu, const QString &id,const QString &text);
+	QAction* newManagedAction(QWidget* menu, const QString &id,const QString &text, const char* slotName, const QList<QKeySequence> &shortCuts = QList<QKeySequence>(), const QString & iconFile="");
+	QAction* newManagedAction(QWidget* menu, const QString &id, QAction* act);
+	QAction* getManagedAction(QString id);
+	QMenu* getManagedMenu(QString id);
+
+	void modifyManagedShortcuts();
+	void loadManagedMenu(QMenu* parent,const QDomElement &f);
+	void loadManagedMenus(const QString &f);
+	void managedMenuToTreeWidget(QTreeWidgetItem* parent, QMenu* menu);
+	void treeWidgetToManagedMenuTo(QTreeWidgetItem* item);
 	
 //private:
 	QString configFileName,configFileNameBase;
