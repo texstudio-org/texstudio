1b680e216faf42a2d53e3461b52a5d4154337458
benibela <benito@benibela.de>
2009-05-25 16:48:20 +0000

    add toolwidgets.cpp
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@228 92cce386-e96d-4106-809f-596c673a3fd8

---
+++ toolwidgets.h
@@ -0,0 +1,75 @@
+#ifndef TOOLWIDGETS_H
+#define TOOLWIDGETS_H
+
+#include "logeditor.h"
+#include "latexlog.h"
+
+
+#include <QDockWidget>
+#include <QLabel>
+#include <QScrollArea>
+#include <QStackedWidget>
+#include <QTabBar>
+#include <QTableView>
+#include <QWidget>
+
+class PreviewWidget : public QScrollArea
+{
+	Q_OBJECT
+public:
+    PreviewWidget(QWidget * parent = 0);
+
+public slots:	
+	void previewLatex(const QPixmap& previewImage);
+	void fitImage();
+	void scaleImage(double factor);
+	void zoomOut();
+	void zoomIn();
+	void contextMenu(QPoint point);
+
+private:
+	QLabel *preViewer;
+	double pvscaleFactor;
+};
+
+class OutputViewWidget: public QDockWidget{
+	Q_OBJECT
+public:
+	OutputViewWidget(QWidget * parent = 0);
+	
+
+	LatexLogModel* getLogModel();
+	void loadLogFile(const QString &logname, const QString & compiledFileName, const QString & overrideFileMName);
+	bool logPresent();
+	void setMessage(const QString &message); //set the message text (don't change page and no auto-show)
+	void insertMessageLine(const QString &message); //inserts the message text (don't change page and no auto-show)
+public slots:
+	void resetMessages(); //remove all messages and jumps to the message page (stays hidden if not visible)
+	void resetMessagesAndLog();
+	void selectLogEntry(int logEntryNumber, bool makeVisible=true);
+	void showLogOrErrorList(); //this will show the log unless the error list is open
+	void showErrorListOrLog(); //this will show the error list unless log is open
+	void showPreview();
+	void gotoLogEntry(int logEntryNumber);
+	void setTabbedLogView(bool tabbed);
+	void previewLatex(const QPixmap& pixmap);
+signals:
+	void locationActivated(int line, QString fileName); //0-based line, absolute file name
+	void logEntryActivated(int logEntryNumber);
+	void tabChanged(int tab);
+private:
+	PreviewWidget *previewWidget;
+	QTableView *OutputTable, *OutputTable2;
+	LogEditor *OutputTextEdit,*OutputLogTextEdit;	
+	QTabBar *logViewerTabBar; //header to select outp (if tabbedLogView, then it is OutputView's TitleBarWidget)
+	QStackedWidget*	OutputLayout;
+	//Latex errors
+	LatexLogModel * logModel; 
+	bool logpresent, tabbedLogView;
+private slots:
+	void clickedOnLogModelIndex(const QModelIndex& index);
+	void gotoLogLine(int logLine);
+};
+
+
+#endif 
