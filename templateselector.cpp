4ccc3b1f9caf05e2cbfca9017597aaeb335ad6c5
jsundermeyer <sunderme@gmx.de>
2009-08-21 16:31:09 +0000

    double click accepts template in templateselector
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@428 92cce386-e96d-4106-809f-596c673a3fd8

--- templateselector.cpp
+++ templateselector.cpp
@@ -22,3 +22,8 @@ templateselector::templateselector(QWidget *parent, QString name)
 
 templateselector::~templateselector() {
 }
+
+void templateselector::on_listWidget_itemDoubleClicked(QListWidgetItem* item)
+{
+	accept();
+}
