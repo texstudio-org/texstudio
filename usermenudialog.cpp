fbdb2352169cddf88218a7766ba433ab2baa66ca
Benito van der Zander <benito@benibela.de>
2010-06-29 23:35:49 +0000

    use qeditor for the user macro menu, with qtscript syntax highlighting
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@1006 92cce386-e96d-4106-809f-596c673a3fd8

--- usermenudialog.cpp
+++ usermenudialog.cpp
@@ -11,9 +11,18 @@
 
 #include "usermenudialog.h"
 
+#include "qcodeedit.h"
+#include "qeditor.h"
+#include "qlanguagefactory.h"
+#include "qlinemarkpanel.h"
+#include "qlinenumberpanel.h"
+#include "qstatuspanel.h"
+#include "qgotolinepanel.h"
+#include "qsearchreplacepanel.h"
+#include "qfoldpanel.h"
 
-UserMenuDialog::UserMenuDialog(QWidget* parent,  QString name)
-		: QDialog(parent) {
+UserMenuDialog::UserMenuDialog(QWidget* parent,  QString name, QLanguageFactory* languageFactory)
+		: QDialog(parent), languages(languageFactory) {
 	setWindowTitle(name);
 	setModal(true);
 	ui.setupUi(this);
@@ -38,58 +47,90 @@ UserMenuDialog::UserMenuDialog(QWidget* parent,  QString name)
 
 	connect(ui.okButton, SIGNAL(clicked()), SLOT(slotOk()));
 
-	connect(ui.tagEdit, SIGNAL(textChanged()), SLOT(textChanged()));
 	connect(ui.radioButtonNormal, SIGNAL(clicked()), SLOT(changeTypeToNormal()));
 	connect(ui.radioButtonEnvironment, SIGNAL(clicked()), SLOT(changeTypeToEnvironment()));
 	connect(ui.radioButtonScript, SIGNAL(clicked()), SLOT(changeTypeToScript()));
+
+	//editor options
+	ui.tagEdit->setLayout(new QVBoxLayout());
+	codeedit = new QCodeEdit(ui.tagEdit);
+	QLineMarkPanel* lineMarkPanel=new QLineMarkPanel;
+	QAction* lineMarkPanelAction=codeedit->addPanel(lineMarkPanel, QCodeEdit::West, false);
+	QLineNumberPanel* lineNumberPanel=new QLineNumberPanel;
+	QAction* lineNumberPanelAction=codeedit->addPanel(lineNumberPanel, QCodeEdit::West, false);;
+	QAction* lineFoldPanelAction=codeedit->addPanel(new QFoldPanel, QCodeEdit::West, false);
+	//QAction* vlineChangePanelAction=codeedit->addPanel(new QLineChangePanel, QCodeEdit::West, false);
+
+	QStatusPanel* statusPanel=new QStatusPanel;
+	statusPanel->setFont(QApplication::font());
+	QAction* statusPanelAction=codeedit->addPanel(statusPanel, QCodeEdit::South, false);
+
+	QGotoLinePanel* gotoLinePanel=new QGotoLinePanel;
+	gotoLinePanel->setFont(QApplication::font());
+	QAction* gotoLinePanelAction=codeedit->addPanel(gotoLinePanel, QCodeEdit::South, false);
+
+	QSearchReplacePanel* searchReplacePanel=new QSearchReplacePanel;
+	searchReplacePanel->setFont(QApplication::font());
+	QAction* searchReplacePanelAction=codeedit->addPanel(searchReplacePanel, QCodeEdit::South,false);
+
+	ui.tagEdit->layout()->addWidget(codeedit->editor());
+
+	connect(codeedit->editor()->document(), SIGNAL(contentsChanged()), SLOT(textChanged()));
 }
 
 UserMenuDialog::~UserMenuDialog() {
+	delete codeedit;
 }
 
 void UserMenuDialog::init() {
-	ui.tagEdit->setPlainText(Tag[0]);
+	codeedit->editor()->setText(Tag[0]);
 	ui.itemEdit->setText(Name[0]);
-        ui.abbrevEdit->setText(Abbrev[0]);
+	ui.abbrevEdit->setText(Abbrev[0]);
 	ui.comboBox->setCurrentIndex(0);
+	if (languages) languages->setLanguage(codeedit->editor(), codeedit->editor()->text(0)!="%SCRIPT"?".tex":".qs");
 }
 
 void UserMenuDialog::change(int index) {
-	Tag[previous_index]=ui.tagEdit->toPlainText();
+	Tag[previous_index]=codeedit->editor()->text();
 	Name[previous_index]=ui.itemEdit->text();
-	ui.tagEdit->setPlainText(Tag[index]);
+	codeedit->editor()->setText(Tag[index]);
 	ui.itemEdit->setText(Name[index]);
-        ui.abbrevEdit->setText(Abbrev[index]);
+	ui.abbrevEdit->setText(Abbrev[index]);
 	previous_index=index;
+	if (languages) languages->setLanguage(codeedit->editor(), codeedit->editor()->text(0)!="%SCRIPT"?".tex":".qs");
 }
 
 void UserMenuDialog::slotOk() {
-	Tag[previous_index]=ui.tagEdit->toPlainText();
+	Tag[previous_index]=codeedit->editor()->text();
 	Name[previous_index]=ui.itemEdit->text();
-        Abbrev[previous_index]=ui.abbrevEdit->text();
+	Abbrev[previous_index]=ui.abbrevEdit->text();
 	accept();
 }
 void UserMenuDialog::changeTypeToNormal(){
-	QString cur = ui.tagEdit->document()->toPlainText();
-	if (cur.startsWith("%SCRIPT\n")) ui.tagEdit->document()->setPlainText(cur.mid(8));
-	else if (cur.startsWith("%")) ui.tagEdit->document()->setPlainText(cur.mid(1));
+	QString cur = codeedit->editor()->text();
+	if (languages) languages->setLanguage(codeedit->editor(), ".tex");
+	if (cur.startsWith("%SCRIPT\n")) codeedit->editor()->setText(cur.mid(8));
+	else if (cur.startsWith("%")) codeedit->editor()->setText(cur.mid(1));
 }
 void UserMenuDialog::changeTypeToEnvironment(){
-	QString cur = ui.tagEdit->document()->toPlainText();
-	if (cur.startsWith("%SCRIPT")) ui.tagEdit->document()->setPlainText("%"+cur.mid(8));
-	else {
+	QString cur = codeedit->editor()->text();
+	if (languages) languages->setLanguage(codeedit->editor(), ".tex");
+	if (cur.startsWith("%SCRIPT")) {
+		codeedit->editor()->setText("%"+cur.mid(8));
+	} else {
 		if (cur.startsWith("%")) return;
-		ui.tagEdit->document()->setPlainText("%"+cur);
+		codeedit->editor()->setText("%"+cur);
 	}
 }
 void UserMenuDialog::changeTypeToScript(){
-	QString cur = ui.tagEdit->document()->toPlainText();
+	QString cur = codeedit->editor()->text();
+	if (languages) languages->setLanguage(codeedit->editor(), ".qs");
 	if (cur.startsWith("%SCRIPT\n")) return;
 	if (cur.startsWith("%")) cur = cur.mid(1);
-	ui.tagEdit->document()->setPlainText("%SCRIPT\n"+cur);
+	codeedit->editor()->setText("%SCRIPT\n"+cur);
 }
 void UserMenuDialog::textChanged(){
-	QString line = ui.tagEdit->document()->firstBlock().text();
+	QString line = codeedit->editor()->text(0);
 	if (line=="%SCRIPT") ui.radioButtonScript->setChecked(true);
 	else if (line.startsWith("%")) ui.radioButtonEnvironment->setChecked(true);
 	else ui.radioButtonNormal->setChecked(true);
