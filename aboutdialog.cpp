248f2c1bac53bc66839592f6dd30735f19632460
jsundermeyer <sunderme@gmx.de>
2010-07-12 17:27:02 +0000

    about dialog translateable
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@1029 92cce386-e96d-4106-809f-596c673a3fd8

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -22,13 +22,13 @@ AboutDialog::AboutDialog(QWidget *parent)
 		:QDialog(parent) {
 	ui.setupUi(this);
 	ui.textBrowser->setOpenExternalLinks(true);
-	ui.textBrowser->setHtml(QString::fromUtf8("<b>TexMakerX 1.9.9a (SVN " SVN_REVISION_NUMBER ")</b><br>Copyright (c) (original TexMaker) 2004-2008 by Pascal Brachet<br>")+
-	                        QString::fromUtf8("TexMakerX: Benito van der Zander, Jan Sundermeyer, Daniel Braun<br>QCodeEdit: Luc Bruant <br>html conversion: Joël Amblard.</i><br>")+
-	                        QString::fromUtf8("TexMakerX contains code from the Hunspell (GPL), QtCreator (GPL, Copyright (C) Nokia) and KILE (GPL) program.<br> TexMakerX uses the DSingleApplication class (Author: Dima Fedorov Levit - Copyright (C) BioImage Informatics - Licence: GPL)<br>")+
-				QString::fromUtf8("Thanks to Frederic Devernay, Denis Bitouzé, Jean-Côme Charpentier, Luis Silvestre, Enrico Vittorini, Aleksandr Zolotarev, David Sichau, Grigory Mozhaev, mattgk, Pavel Fric, András Somogyi & István Blahota.<br><br>")+
-	                        QString::fromUtf8("Project home site : <a href=\"http://texmakerx.sourceforge.net/\">http://texmakerx.sourceforge.net/</a><br><br>")+
-	                        QString::fromUtf8("Home site of original Texmaker: <a href=\"http://www.xm1math.net/texmaker/\">http://www.xm1math.net/texmaker/</a><br><br>")+
-	                        QString::fromUtf8("This program is licensed to you under the terms of the GNU General Public License Version 2 as published by the Free Software Foundation."));
+	ui.textBrowser->setHtml(tr("<b>TexMakerX 1.9.9a (SVN " SVN_REVISION_NUMBER ")</b><br>Copyright (c) (original TexMaker) 2004-2008 by Pascal Brachet<br>")+
+							tr("TexMakerX: Benito van der Zander, Jan Sundermeyer, Daniel Braun<br>QCodeEdit: Luc Bruant <br>html conversion: ")+QString::fromUtf8("Joël Amblard.</i><br>")+
+							tr("TexMakerX contains code from the Hunspell (GPL), QtCreator (GPL, Copyright (C) Nokia) and KILE (GPL) program.<br> TexMakerX uses the DSingleApplication class (Author: Dima Fedorov Levit - Copyright (C) BioImage Informatics - Licence: GPL)<br>")+
+							tr("Thanks to ")+QString::fromUtf8("Frederic Devernay, Denis Bitouzé, Jean-Côme Charpentier, Luis Silvestre, Enrico Vittorini, Aleksandr Zolotarev, David Sichau, Grigory Mozhaev, mattgk, Pavel Fric, András Somogyi & István Blahota.<br><br>")+
+							tr("Project home site : <a href=\"http://texmakerx.sourceforge.net/\">http://texmakerx.sourceforge.net/</a><br><br>")+
+							tr("Home site of original Texmaker: <a href=\"http://www.xm1math.net/texmaker/\">http://www.xm1math.net/texmaker/</a><br><br>")+
+							tr("This program is licensed to you under the terms of the GNU General Public License Version 2 as published by the Free Software Foundation."));
 }
 
 AboutDialog::~AboutDialog() {
