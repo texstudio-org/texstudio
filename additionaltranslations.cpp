9675f3229e9bfa69c43b76db405053e42e9be1a5
jsundermeyer <14033169+sunderme@users.noreply.github.com>
2010-08-31 07:49:26 +0000

    toolbar name "Table" now translateable
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@1159 92cce386-e96d-4106-809f-596c673a3fd8

--- additionaltranslations.cpp
+++ additionaltranslations.cpp
@@ -1,146 +1,292 @@
 #undef UNDEFINED
-#ifdef UNDEFINED
-static const char* translations[] = {
-QT_TRANSLATE_NOOP("ConfigManager", "&Latex"), 
-QT_TRANSLATE_NOOP("ConfigManager", "AMS packages"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{document}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "&Sectioning"), 
-QT_TRANSLATE_NOOP("ConfigManager", "part"), 
-QT_TRANSLATE_NOOP("ConfigManager", "chapter"), 
-QT_TRANSLATE_NOOP("ConfigManager", "section"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subsection"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subsubsection"), 
-QT_TRANSLATE_NOOP("ConfigManager", "paragraph"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subparagraph"), 
-QT_TRANSLATE_NOOP("ConfigManager", "part*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "chapter*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "section*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subsection*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subsubsection*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "paragraph*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subparagraph*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "&Environments"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{<environment>} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{center} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{flushleft} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{flushright}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{quote}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{quotation}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{verse}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{verbatim}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{table}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{figure}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{titlepage}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "&List Environments"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{itemize}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{enumerate}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{description}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{list}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Font St&yles"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\emph - Emphasis  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\textit - Italics  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\textsl - Slanted  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\textbf - Boldface  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\texttt - Typewriter  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\textsc - Small caps  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\textsf - Sans Serif  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\underline - Underline [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Font Sizes"), 
-QT_TRANSLATE_NOOP("ConfigManager", "tiny"), 
-QT_TRANSLATE_NOOP("ConfigManager", "scriptsize"), 
-QT_TRANSLATE_NOOP("ConfigManager", "footnotesize"), 
-QT_TRANSLATE_NOOP("ConfigManager", "small"), 
-QT_TRANSLATE_NOOP("ConfigManager", "normalsize"), 
-QT_TRANSLATE_NOOP("ConfigManager", "large"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Large"), 
-QT_TRANSLATE_NOOP("ConfigManager", "LARGE"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Huge"), 
-QT_TRANSLATE_NOOP("ConfigManager", "&Tabular Environment"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{tabbing}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{tabular}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "S&pacing"), 
-QT_TRANSLATE_NOOP("ConfigManager", "New line"), 
-QT_TRANSLATE_NOOP("ConfigManager", "International &Accents"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\includegraphics{file}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\include{file}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\input{file}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Cross References"), 
-QT_TRANSLATE_NOOP("ConfigManager", "label"), 
-QT_TRANSLATE_NOOP("ConfigManager", "ref"), 
-QT_TRANSLATE_NOOP("ConfigManager", "pageref"), 
-QT_TRANSLATE_NOOP("ConfigManager", "index"), 
-QT_TRANSLATE_NOOP("ConfigManager", "cite"), 
-QT_TRANSLATE_NOOP("ConfigManager", "footnote"), 
-QT_TRANSLATE_NOOP("ConfigManager", "&Math"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Inline math mode $...$"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Display math mode \\[...\\]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Numbered equations \\begin{equation}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{align}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{eqnarray}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "_{} - subscript"), 
-QT_TRANSLATE_NOOP("ConfigManager", "^{} - superscript"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{array}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Math &Functions"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Math Font St&yles"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathrm{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathit{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathbf{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathsf{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathtt{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathcal{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathbb{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathfrak{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Math &Accents"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Math S&paces"), 
-QT_TRANSLATE_NOOP("ConfigManager", "small"), 
-QT_TRANSLATE_NOOP("ConfigManager", "medium"), 
-QT_TRANSLATE_NOOP("ConfigManager", "large"), 
-QT_TRANSLATE_NOOP("ConfigManager", "quad"), 
-QT_TRANSLATE_NOOP("ConfigManager", "qquad"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "background"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:error"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:warning"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:badbox"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark0"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark1"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark2"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark3"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark4"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark5"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark6"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark7"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark8"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark9"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "comment"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "keyword"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "extra-keyword"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "math-keyword"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "verbatim"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "braceMatch"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "braceMismatch"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "search"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "numbers"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "text"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "escapeseq"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "spellingMistake"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "temporaryCodeCompletion"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "environment"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "referencePresent"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "referenceMissing"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "referenceMultiple"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "citationPresent"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "citationMissing"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "current"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "selection"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "replacement"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "normal"),
-QT_TRANSLATE_NOOP("Texmaker","Custom"),
-QT_TRANSLATE_NOOP("Texmaker","File"),
-QT_TRANSLATE_NOOP("Texmaker","Edit"),
-QT_TRANSLATE_NOOP("Texmaker","Tools"),
-QT_TRANSLATE_NOOP("Texmaker","Math"),
-QT_TRANSLATE_NOOP("Texmaker","Format"),
-""};
-#endif
+
+#ifdef UNDEFINED
+
+static const char* translations[] = {
+
+QT_TRANSLATE_NOOP("ConfigManager", "&Latex"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "AMS packages"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{document}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "&Sectioning"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "part"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "chapter"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "section"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subsection"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subsubsection"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "paragraph"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subparagraph"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "part*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "chapter*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "section*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subsection*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subsubsection*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "paragraph*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subparagraph*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "&Environments"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{<environment>} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{center} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{flushleft} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{flushright}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{quote}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{quotation}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{verse}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{verbatim}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{table}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{figure}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{titlepage}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "&List Environments"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{itemize}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{enumerate}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{description}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{list}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Font St&yles"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\emph - Emphasis  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\textit - Italics  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\textsl - Slanted  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\textbf - Boldface  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\texttt - Typewriter  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\textsc - Small caps  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\textsf - Sans Serif  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\underline - Underline [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Font Sizes"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "tiny"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "scriptsize"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "footnotesize"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "small"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "normalsize"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "large"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Large"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "LARGE"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Huge"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "&Tabular Environment"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{tabbing}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{tabular}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "S&pacing"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "New line"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "International &Accents"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\includegraphics{file}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\include{file}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\input{file}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Cross References"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "label"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "ref"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "pageref"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "index"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "cite"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "footnote"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "&Math"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Inline math mode $...$"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Display math mode \\[...\\]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Numbered equations \\begin{equation}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{align}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{eqnarray}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "_{} - subscript"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "^{} - superscript"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{array}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Math &Functions"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Math Font St&yles"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathrm{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathit{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathbf{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathsf{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathtt{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathcal{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathbb{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathfrak{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Math &Accents"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Math S&paces"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "small"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "medium"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "large"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "quad"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "qquad"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "background"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:error"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:warning"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:badbox"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark0"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark1"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark2"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark3"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark4"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark5"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark6"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark7"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark8"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark9"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "comment"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "keyword"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "extra-keyword"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "math-keyword"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "verbatim"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "braceMatch"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "braceMismatch"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "search"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "numbers"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "text"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "escapeseq"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "spellingMistake"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "temporaryCodeCompletion"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "environment"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "referencePresent"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "referenceMissing"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "referenceMultiple"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "citationPresent"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "citationMissing"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "current"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "selection"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "replacement"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "normal"),
+
+QT_TRANSLATE_NOOP("Texmaker","Custom"),
+
+QT_TRANSLATE_NOOP("Texmaker","File"),
+
+QT_TRANSLATE_NOOP("Texmaker","Edit"),
+
+QT_TRANSLATE_NOOP("Texmaker","Tools"),
+
+QT_TRANSLATE_NOOP("Texmaker","Math"),
+
+QT_TRANSLATE_NOOP("Texmaker","Format"),
+
+QT_TRANSLATE_NOOP("Texmaker","Table"),
+
+""};
+
+#endif
 
