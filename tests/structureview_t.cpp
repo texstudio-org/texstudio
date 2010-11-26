55602892c3307e0f8e6b8a40350bf7bf68e2b5ca
benibela <benito@benibela.de>
2010-11-26 15:03:23 +0000

    fix wrong assumption about reused structure elements, which could lead to a crash if sections of different levels are on the same line
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@1467 92cce386-e96d-4106-809f-596c673a3fd8

--- tests/structureview_t.cpp
+++ tests/structureview_t.cpp
@@ -13,146 +13,208 @@ StructureViewTest::StructureViewTest(QEditor* editor,LatexDocument *doc): ed(edi
 
 void StructureViewTest::script_data(){
 	QTest::addColumn<QString>("script");
-	
+	QTest::addColumn<QString>("expectedStructure");
+
 	//-------------cursor without selection--------------
 	QTest::newRow("Setup Text")
-		<< "editor.setText(\"Hallo\")";
+		<< "editor.setText(\"Hallo\")"
+		<<"Root: LVL:0 IND:0" ;
 
 	QTest::newRow("add Label")
-		<< "cursor.movePosition(1,cursorEnums.End);cursor.insertText(\"\\n \\\\label{test}\\n\")";
+		<< "cursor.movePosition(1,cursorEnums.End);cursor.insertText(\"\\n \\\\label{test}\\n\")"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2"
+		;
 
 	QTest::newRow("add Label2")
-		<< "cursor.insertText(\" \\\\label{test2}\\n\")";
+		<< "cursor.insertText(\" \\\\label{test2}\\n\")"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test2 LVL:0 IND:2" ;
 
 	QTest::newRow("add Label3")
-		<< "cursor.insertText(\" \\\\label{test3}\\n\");cursor.insertText(\" \\\\label{test4}\\n\");cursor.insertText(\" \\\\label{test5}\\n\");cursor.insertText(\" \\\\label{test6}\\n\")";
+		<< "cursor.insertText(\" \\\\label{test3}\\n\");cursor.insertText(\" \\\\label{test4}\\n\");cursor.insertText(\" \\\\label{test5}\\n\");cursor.insertText(\" \\\\label{test6}\\n\")"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test2 LVL:0 IND:2##Label:test3 LVL:0 IND:2##Label:test4 LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:test6 LVL:0 IND:2" ;
 
 	QTest::newRow("change Label")
-		<< "cursor.movePosition(1,cursorEnums.Up);cursor.movePosition(9,cursorEnums.Right);cursor.insertText(\"a\");cursor.insertText(\"b\");cursor.insertText(\"c\")";
+		<< "cursor.movePosition(1,cursorEnums.Up);cursor.movePosition(9,cursorEnums.Right);cursor.insertText(\"a\");cursor.insertText(\"b\");cursor.insertText(\"c\")"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test2 LVL:0 IND:2##Label:test3 LVL:0 IND:2##Label:test4 LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2" ;
 
 	QTest::newRow("change Label2")
-		<< "cursor.movePosition(1,cursorEnums.StartOfLine);cursor.movePosition(3,cursorEnums.Up);cursor.movePosition(9,cursorEnums.Right);cursor.insertText(\"a\");cursor.insertText(\"b\");cursor.insertText(\"c\")";
+		<< "cursor.movePosition(1,cursorEnums.StartOfLine);cursor.movePosition(3,cursorEnums.Up);cursor.movePosition(9,cursorEnums.Right);cursor.insertText(\"a\");cursor.insertText(\"b\");cursor.insertText(\"c\")"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test2 LVL:0 IND:2##Label:tabcest3 LVL:0 IND:2##Label:test4 LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2";
+
 
 	QTest::newRow("change Label3")
-		<< "cursor.deleteChar()";
+		<< "cursor.deleteChar()"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test2 LVL:0 IND:2##Label:tabcst3 LVL:0 IND:2##Label:test4 LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2" ;
 
 	QTest::newRow("remove line")
-		<< "cursor.eraseLine()";
+		<< "cursor.eraseLine()"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test2 LVL:0 IND:2##Label:test4 LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2" ;
 
 	QTest::newRow("remove lines2")
-		<< "cursor.movePosition(1,cursorEnums.EndOfLine);cursor.movePosition(1,cursorEnums.StartOfLine,cursorEnums.KeepAnchor);cursor.movePosition(1,cursorEnums.Up,cursorEnums.KeepAnchor);cursor.removeSelectedText()";
+		<< "cursor.movePosition(1,cursorEnums.EndOfLine);cursor.movePosition(1,cursorEnums.StartOfLine,cursorEnums.KeepAnchor);cursor.movePosition(1,cursorEnums.Up,cursorEnums.KeepAnchor);cursor.removeSelectedText()"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2" ;
 
 	QTest::newRow("add section")
-			<< "cursor.movePosition(1,cursorEnums.End);cursor.insertText(\" \\\\section{sec:test}\\n\")";
+		<< "cursor.movePosition(1,cursorEnums.End);cursor.insertText(\" \\\\section{sec:test}\\n\")"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2##Section:sec:test LVL:2 IND:1" ;
 
 	QTest::newRow("add section2")
-			<< "cursor.movePosition(1,cursorEnums.End);cursor.insertText(\" \\\\section{sec:test}\\n\")";
+		<< "cursor.movePosition(1,cursorEnums.End);cursor.insertText(\" \\\\section{sec:test}\\n\")"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2##Section:sec:test LVL:2 IND:1##Section:sec:test LVL:2 IND:1" ;
 
 	QTest::newRow("change section")
-		<< "cursor.movePosition(1,cursorEnums.Up);cursor.movePosition(11,cursorEnums.Right);cursor.insertText(\"a\");cursor.insertText(\"b\");cursor.insertText(\"c\")";
+		<< "cursor.movePosition(1,cursorEnums.Up);cursor.movePosition(11,cursorEnums.Right);cursor.insertText(\"a\");cursor.insertText(\"b\");cursor.insertText(\"c\")"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2##Section:sec:test LVL:2 IND:1##Section:sabcec:test LVL:2 IND:1" ;
 
 	QTest::newRow("change section2")
-		<< "cursor.movePosition(1,cursorEnums.StartOfLine);cursor.movePosition(2,cursorEnums.Right);cursor.insertText(\"sub\")";
+		<< "cursor.movePosition(1,cursorEnums.StartOfLine);cursor.movePosition(2,cursorEnums.Right);cursor.insertText(\"sub\")"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2##Section:sec:test LVL:2 IND:1##Section:sabcec:test LVL:3 IND:2" ;
 
 	QTest::newRow("change section3")
-		<< "cursor.movePosition(1,cursorEnums.StartOfLine);cursor.movePosition(2,cursorEnums.Right);cursor.insertText(\"sub\")";
+		<< "cursor.movePosition(1,cursorEnums.StartOfLine);cursor.movePosition(2,cursorEnums.Right);cursor.insertText(\"sub\")"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2##Section:sec:test LVL:2 IND:1##Section:sabcec:test LVL:4 IND:2" ;
 
 	QTest::newRow("change section4")
-		<< "cursor.movePosition(1,cursorEnums.StartOfLine);cursor.movePosition(2,cursorEnums.Right);cursor.movePosition(6,cursorEnums.Right,cursorEnums.KeepAnchor);cursor.removeSelectedText()";
+		<< "cursor.movePosition(1,cursorEnums.StartOfLine);cursor.movePosition(2,cursorEnums.Right);cursor.movePosition(6,cursorEnums.Right,cursorEnums.KeepAnchor);cursor.removeSelectedText()"
+		<< "Root: LVL:0 IND:0##Overview:LABELS LVL:0 IND:1##Label:test LVL:0 IND:2##Label:test5 LVL:0 IND:2##Label:tabcest6 LVL:0 IND:2##Section:sec:test LVL:2 IND:1##Section:sabcec:test LVL:2 IND:1" ;
 
 	QTest::newRow("set sequence of headings")
-			<< "editor.setText(\"\\\\section{a}\\n\\\\section{b}\\n\\\\section{c}\\n\")";
+			<< "editor.setText(\"\\\\section{a}\\n\\\\section{b}\\n\\\\section{c}\\n\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:2 IND:1##Section:c LVL:2 IND:1" ;
 
 	QTest::newRow("change heading in line 1 up")
-			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\chapter{a}\")";
+			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\chapter{a}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:1 IND:1##Section:b LVL:2 IND:2##Section:c LVL:2 IND:2" ;
 
 	QTest::newRow("change heading in line 1 up")
-			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\subsection{a}\")";
+			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\subsection{a}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:3 IND:1##Section:b LVL:2 IND:1##Section:c LVL:2 IND:1" ;
 
 	QTest::newRow("change heading in line 1 back")
-			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\section{a}\")";
+			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\section{a}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:2 IND:1##Section:c LVL:2 IND:1" ;
 
 	QTest::newRow("change heading in line 2 up")
-			<< "cursor.moveTo(1,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\chapter{b}\")";
+			<< "cursor.moveTo(1,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\chapter{b}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:1 IND:1##Section:c LVL:2 IND:2" ;
 
 	QTest::newRow("change heading in line 1 (remove)")
-			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\ssection{a}\")";
+			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\ssection{a}\")"
+			<< "Root: LVL:0 IND:0##Section:b LVL:1 IND:1##Section:c LVL:2 IND:2" ;
 
 	QTest::newRow("change heading in line 1 (add)")
-			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\section{a}\")";
+			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\section{a}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:1 IND:1##Section:c LVL:2 IND:2" ;
 
 	QTest::newRow("change heading in line 2 down")
-			<< "cursor.moveTo(1,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\subsection{b}\")";
+			<< "cursor.moveTo(1,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\subsection{b}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:3 IND:2##Section:c LVL:2 IND:1" ;
 
 	QTest::newRow("change heading in line 2 back")
-			<< "cursor.moveTo(1,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\section{b}\")";
+			<< "cursor.moveTo(1,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\section{b}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:2 IND:1##Section:c LVL:2 IND:1" ;
 
 	QTest::newRow("change heading in line 3 up")
-			<< "cursor.moveTo(2,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\chapter{c}\")";
+			<< "cursor.moveTo(2,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\chapter{c}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:2 IND:1##Section:c LVL:1 IND:1" ;
 
 	QTest::newRow("change heading in line 3 down")
-			<< "cursor.moveTo(2,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\subsection{c}\")";
+			<< "cursor.moveTo(2,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\subsection{c}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:2 IND:1##Section:c LVL:3 IND:2" ;
 
 	QTest::newRow("change heading in line 3 back")
-			<< "cursor.moveTo(2,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\section{c}\")";
+			<< "cursor.moveTo(2,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\section{c}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:2 IND:1##Section:c LVL:2 IND:1" ;
 
 	QTest::newRow("set sequence of sections/subsection")
-			<< "editor.setText(\"\\\\section{a}\\n\\\\subsection{sdfgsdfgsdfgsfgb}\\n\\\\subsection{c}\\n\")";
+			<< "editor.setText(\"\\\\section{a}\\n\\\\subsection{sdfgsdfgsdfgsfgb}\\n\\\\subsection{c}\\n\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:sdfgsdfgsdfgsfgb LVL:3 IND:2##Section:c LVL:3 IND:2" ;
 
 	QTest::newRow("change heading in line 2 down")
-			<< "cursor.moveTo(1,0);cursor.movePosition(1,cursorEnums.EndOfLine);cursor.movePosition(3,cursorEnums.Left);cursor.insertText(\"df\")";
+			<< "cursor.moveTo(1,0);cursor.movePosition(1,cursorEnums.EndOfLine);cursor.movePosition(3,cursorEnums.Left);cursor.insertText(\"df\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:sdfgsdfgsdfgsfdfgb LVL:3 IND:2##Section:c LVL:3 IND:2" ;
 
 	QTest::newRow("set sequence of sections/chapter")
-			<< "editor.setText(\"\\\\section{a}\\n\\\\section{b}\\n\\\\chapter{c}\\n\")";
+			<< "editor.setText(\"\\\\section{a}\\n\\\\section{b}\\n\\\\chapter{c}\\n\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:2 IND:1##Section:c LVL:1 IND:1" ;
 
 	QTest::newRow("change heading in line 2 up")
-			<< "cursor.moveTo(1,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\chapter{b}\")";
+			<< "cursor.moveTo(1,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\chapter{b}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:1 IND:1##Section:c LVL:1 IND:1" ;
 
 	QTest::newRow("set sequence of sections/subsec")
-			<< "editor.setText(\"\\\\section{a}\\n\\\\subsection{b}\\n\")";
+			<< "editor.setText(\"\\\\section{a}\\n\\\\subsection{b}\\n\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:3 IND:2" ;
 
 	QTest::newRow("change heading in line 2 up")
-			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\chapter{a}\")";
+			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\chapter{a}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:1 IND:1##Section:b LVL:3 IND:2" ;
 
 	QTest::newRow("set sequence of subsection/section")
-			<< "editor.setText(\"\\\\subsection{a}\\n\\\\subsection{b}\\n\\\\section{c}\\n\")";
+			<< "editor.setText(\"\\\\subsection{a}\\n\\\\subsection{b}\\n\\\\section{c}\\n\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:3 IND:1##Section:b LVL:3 IND:1##Section:c LVL:2 IND:1" ;
 
 	QTest::newRow("change first subsection to section")
-			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\section{a}\")";
+			<< "cursor.moveTo(0,0);cursor.movePosition(1,cursorEnums.EndOfLine,cursorEnums.KeepAnchor);cursor.replaceSelectedText(\"\\\\section{a}\")"
+			<< "Root: LVL:0 IND:0##Section:a LVL:2 IND:1##Section:b LVL:3 IND:2##Section:c LVL:2 IND:1" ;
+
+	QTest::newRow("Nested structure on one line")
+			<< "editor.setText(\"123\\\\chapter{abc}...\\\\section{def},,,\\\\chapter{xyz}\")"
+			<< "Root: LVL:0 IND:0##Section:abc LVL:1 IND:1##Section:def LVL:2 IND:2##Section:xyz LVL:1 IND:1";
+
 }
 
 void StructureViewTest::script(){
 	QFETCH(QString, script);
-	
+	QFETCH(QString, expectedStructure);
+
 	scriptengine eng(this);
 	eng.setEditor(ed);
 	eng.setScript(script);
 	eng.run();
 
 	QStringList res1=unrollStructure(document->baseStructure);
+	QCOMPARE(res1.join("##"), expectedStructure);
+//	qDebug() << "Unrolled Structure: " << res1.join("##");
 	document->updateStructure();
 	QStringList res2=unrollStructure(document->baseStructure);
 	QCOMPARE(res1,res2);
+
 	
 }
-QStringList StructureViewTest::unrollStructure(StructureEntry *baseStructure){
-    QStringList result;
-    StructureEntryIterator iter(baseStructure);
-    while (iter.hasNext()) {
-	StructureEntry *se=iter.next();
-	QString line=se->title;
-	line+=QString(" LVL:%1").arg(se->level);
-	int i=0;
-	StructureEntry *l=se;
-	while(l->parent){
-		l=l->parent;
-		i++;
+
+QString structureTypeToString(StructureEntry::Type type) {
+	switch (type) {
+	case StructureEntry::SE_DOCUMENT_ROOT: return "Root";
+	case StructureEntry::SE_OVERVIEW: return "Overview";
+	case StructureEntry::SE_SECTION: return "Section";
+	case StructureEntry::SE_BIBTEX: return "BibTeX";
+	case StructureEntry::SE_TODO: return "Todo";
+	case StructureEntry::SE_INCLUDE: return "Include";
+	case StructureEntry::SE_LABEL: return "Label";
+//	case StructureEntry::SE_BLOCK: return "Block";
+	default: Q_ASSERT(false);
 	}
-	line+=QString(" IND:%1").arg(i);
-	result << line;
-	//qDebug(qPrintable(line));
-    }
-    return result;
+	return "Error";
+}
+
+QStringList StructureViewTest::unrollStructure(StructureEntry *baseStructure){
+	QStringList result;
+	StructureEntryIterator iter(baseStructure);
+	while (iter.hasNext()) {
+		StructureEntry *se=iter.next();
+		QString line=QString("%1:%2 LVL:%3").arg(structureTypeToString(se->type)).arg(se->title).arg(se->level);
+		int i=0;
+		StructureEntry *l=se;
+		while(l->parent){
+			l=l->parent;
+			i++;
+		}
+		line+=QString(" IND:%1").arg(i);
+		result << line;
+		//qDebug(qPrintable(line));
+	}
+	return result;
 }
 
 void StructureViewTest::benchmark_data(){
