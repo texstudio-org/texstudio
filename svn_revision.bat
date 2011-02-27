d8dab3026e7957efef66c63fbe220a4d60510138
benibela <benito@benibela.de>
2011-02-27 11:46:10 +0000

    Qt creator windows build
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@1702 92cce386-e96d-4106-809f-596c673a3fd8

--- svn_revision.bat
+++ svn_revision.bat
@@ -1,5 +1,6 @@
+cd %0\.. 
 @echo #define MK_STRING(x) #x > svn_revision.cpp
 @echo const char * TEXMAKERX_SVN_VERSION = MK_STRING(  >> svn_revision.cpp
 call svnversion >> svn_revision.cpp
 @echo ); >> svn_revision.cpp
-%1 -c svn_revision.cpp -o svn_revision.o
+%1 -c svn_revision.cpp -o %2/svn_revision.o
