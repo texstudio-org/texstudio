40e8d4323c5257c9111c7d7114a6fbae5439a7cb
Benito van der Zander <benito@benibela.de>
2011-04-09 14:00:31 +0000

    universal input dialog (float)
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@1864 92cce386-e96d-4106-809f-596c673a3fd8

--- universalinputdialog.h
+++ universalinputdialog.h
@@ -10,16 +10,17 @@ class UniversalInputDialog : public QDialog
 	Q_OBJECT
 private:
 	QGridLayout* gridLayout;
-	void addWidget(QWidget* widget, QString description="", void* storage=0);
+	void addWidget(QWidget* widget, const QString& description="", void* storage=0);
 private slots:
 	void myAccept();
 public:
 	UniversalInputDialog(QWidget* parent=0);
-	void addVariable(bool* var, QString description="");
-	void addVariable(int* var, QString description="");
-	void addVariable(QString* var, QString description="");
-	void addVariable(QStringList* var, QString description="");
-	void addTextEdit(QString* var, QString description="");
+	void addVariable(bool* var, const QString& description="");
+	void addVariable(int* var, const QString& description="");
+	void addVariable(QString* var, const QString& description="");
+	void addVariable(QStringList* var, const QString& description="");
+	void addTextEdit(QString* var, const QString& description="");
+	void addVariable(float* var, const QString& description="");
 	virtual void showEvent(QShowEvent* event);
 };
 
