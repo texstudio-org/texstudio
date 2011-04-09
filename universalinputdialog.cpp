f92dad05dfa96b483125977f35f49234fc1a57e0
benibela <benito@benibela.de>
2011-04-09 14:00:31 +0000

    universal input dialog (float)
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@1864 92cce386-e96d-4106-809f-596c673a3fd8

--- universalinputdialog.cpp
+++ universalinputdialog.cpp
@@ -30,6 +30,11 @@ void UniversalInputDialog::myAccept(){
 				*((QString*)storage) = tedit->toPlainText();
 				continue;
 			}
+			QDoubleSpinBox* doubleSpinBox = qobject_cast<QDoubleSpinBox*>(w);
+			if (doubleSpinBox){
+				*((float*)storage) = doubleSpinBox->value();
+				continue;
+			}
 			QSpinBox* spinBox = qobject_cast<QSpinBox*>(w);
 			if (spinBox){
 				*((int*)storage) = spinBox->value();
@@ -45,7 +50,7 @@ void UniversalInputDialog::myAccept(){
 	accept();
 }
 
-void UniversalInputDialog::addWidget(QWidget* widget, QString description, void* storage){
+void UniversalInputDialog::addWidget(QWidget* widget, const QString& description, void* storage){
 	widget->setProperty("storage", QVariant::fromValue((void*)storage));
 	widget->setSizePolicy(QSizePolicy::Ignored,QSizePolicy::Preferred);
 	QLabel *descWidget = new QLabel(description, this);
@@ -60,7 +65,7 @@ void UniversalInputDialog::addWidget(QWidget* widget, QString description, void*
 	}
 }
 
-void UniversalInputDialog::addVariable(bool* var, QString description){
+void UniversalInputDialog::addVariable(bool* var, const QString& description){
 	Q_ASSERT(var);
 	QCheckBox* checkBox = new QCheckBox(this);
 	checkBox->setText(description);
@@ -68,35 +73,45 @@ void UniversalInputDialog::addVariable(bool* var, QString description){
 	checkBox->setProperty("storage", QVariant::fromValue((void*)var));
 	gridLayout->addWidget(checkBox,gridLayout->rowCount(),1,0);
 }
-void UniversalInputDialog::addVariable(int* var, QString description){
+void UniversalInputDialog::addVariable(int* var, const QString& description){
 	Q_ASSERT(var);
 	QSpinBox* spinBox = new QSpinBox(this);
-	spinBox->setValue(*var);
 	spinBox->setMaximum(10000000);
+	spinBox->setValue(*var);
 	addWidget(spinBox,description,var);
 }
-void UniversalInputDialog::addVariable(QString* var, QString description){
+void UniversalInputDialog::addVariable(QString* var, const QString& description){
 	Q_ASSERT(var);
 	QLineEdit* edit = new QLineEdit(this);
 	edit->setText(*var);
 	addWidget(edit,description,var);
 }
-void UniversalInputDialog::addVariable(QStringList* var, QString description){
+void UniversalInputDialog::addVariable(QStringList* var, const QString& description){
 	Q_ASSERT(var);
 	QComboBox* cmb = new QComboBox(this);
 	cmb->addItems(*var);
 	addWidget(cmb,description,var);
 }
-void UniversalInputDialog::addTextEdit(QString* var, QString description){
+void UniversalInputDialog::addTextEdit(QString* var, const QString& description){
 	Q_ASSERT(var);
 	QTextEdit* edit = new QTextEdit(this);
 	edit->setPlainText(*var);
 	addWidget(edit,description,var);
 }
+void UniversalInputDialog::addVariable(float* var, const QString& description){
+	Q_ASSERT(var);
+	QDoubleSpinBox* spinBox = new QDoubleSpinBox(this);
+	spinBox->setMinimum(-10000000);
+	spinBox->setMaximum(10000000);
+	spinBox->setValue(*var);
+	addWidget(spinBox,description,var);
+}
+
 void UniversalInputDialog::showEvent(QShowEvent* event){
 	QDialog::showEvent(event);
 	if (event->spontaneous()) return;
 	QDialogButtonBox *box=new QDialogButtonBox(QDialogButtonBox::Ok|QDialogButtonBox::Cancel, Qt::Horizontal, this);
+	box->button(QDialogButtonBox::Ok)->setDefault(true);
 	connect(box,SIGNAL(accepted()), this, SLOT(myAccept()));
 	connect(box,SIGNAL(rejected()), this, SLOT(reject()));
 	gridLayout->addWidget(box, gridLayout->rowCount(), 0, 1, 2, 0);
