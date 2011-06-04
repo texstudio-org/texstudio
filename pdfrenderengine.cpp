53152c1d1e9e7f576e264c5d2e8d25a8f9cdad43
Benito van der Zander <benito@benibela.de>
2011-06-04 13:07:53 +0000

    revert 2055
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@2061 92cce386-e96d-4106-809f-596c673a3fd8

--- pdfrenderengine.cpp
+++ pdfrenderengine.cpp
@@ -39,8 +39,7 @@ void PDFRenderEngine::setDocument(Poppler::Document *doc){
 
 
 void PDFRenderEngine::run(){
-	queue->runningQueues.acquire(1);
-	while (!queue->stopped) {
+	forever {
 		bool priorityThread=queue->mPriorityLock.tryLock();
 		RenderCommand command(-1);
 		if(priorityThread){
@@ -89,7 +88,6 @@ void PDFRenderEngine::run(){
 		    //qDebug() << this << " Render page " << command.pageNr << " at " << command.ticket << priorityThread << "x/y" << command.x << command.y << " res "<<command.xres << ", " << command.w << command.h;
 		}
 	}
-	queue->runningQueues.release(1);
 	queue->deref();
 	deleteLater();
 }
