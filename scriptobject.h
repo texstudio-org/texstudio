699c0e9684b2d8138ca970c56f93a0fad8415aef
benibela <benito@benibela.de>
2011-08-08 23:31:38 +0000

    script config access
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@2192 92cce386-e96d-4106-809f-596c673a3fd8

--- scriptobject.h
+++ scriptobject.h
@@ -5,12 +5,37 @@
 #include "qdocumentcursor.h"
 class ProcessX;
 class BuildManager;
+class ScriptObject;
+
+//script object  for texworks compatibility
+class SubScriptObject : public QObject
+{
+	Q_OBJECT
+public:	
+	ScriptObject *script;
+	
+public slots:
+	QByteArray getScriptHash();
+
+	bool hasGlobal(const QString& name);
+	void setGlobal(const QString& name, const QVariant& value);
+	QVariant getGlobal(const QString& name);
+	
+	bool hasPersistent(const QString& name);
+	void setPersistent(const QString& name, const QVariant& value);
+	QVariant getPersistent(const QString& name);
+};
+
+//global scope object
 class ScriptObject : public QObject
 {
 	Q_OBJECT
 
+	Q_PROPERTY(SubScriptObject* script READ getScript DESIGNABLE false STORED false CONSTANT FINAL)
 public:
 	explicit ScriptObject(const QString& script, BuildManager* buildManager);
+
+	QByteArray getScriptHash();
 signals:
 
 public slots:
@@ -24,14 +49,17 @@ public slots:
 	bool hasPrivileges();
 	
 	ProcessX* system(const QString& commandline);
-	
+		
 	void writeFile(const QString& filename, const QString& content);
 	QVariant readFile(const QString& filename);	
+	
 private:
 	const QString& script;
 	BuildManager* buildManager;
 	QByteArray scriptHash;
 	bool needPrivileges(const QString& commandline);
+	SubScriptObject subScriptObject;
+	SubScriptObject* getScript();
 };
 
 #endif // SCRIPTOBJECT_H
