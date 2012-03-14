190da6211c23c118049329b6ea9b2d3d283527b2
benibela <benito@benibela.de>
2012-03-14 22:43:26 +0000

    indentation
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@2650 92cce386-e96d-4106-809f-596c673a3fd8

--- searchresultmodel.cpp
+++ searchresultmodel.cpp
@@ -20,15 +20,15 @@ void SearchResultModel::addSearch(QList<QDocumentLineHandle *>newSearch,QString 
 void SearchResultModel::clear(){
 	m_searches.clear();
 	m_files.clear();
-        mExpression.clear();
+	mExpression.clear();
 	reset();
 }
 
 void SearchResultModel::removeSearch(QString name){
-        int i=m_files.indexOf(name);
+	int i=m_files.indexOf(name);
 	m_searches.removeAt(i);
 	m_files.removeAt(i);
-        if(m_files.isEmpty()) mExpression.clear();
+	if(m_files.isEmpty()) mExpression.clear();
 }
 int SearchResultModel::columnCount(const QModelIndex & parent) const {
 	return parent.isValid()?1:1;
@@ -40,13 +40,13 @@ int SearchResultModel::rowCount(const QModelIndex &parent) const {
 	}else{
 		int i=parent.row();
 		if(i<m_searches.size()&&((parent.internalId()&(1<<15))==0)){
-                        QList<QDocumentLineHandle *> search=m_searches.at(i);
-                        return qMin(search.size(),1000); // maximum search results limited
+			QList<QDocumentLineHandle *> search=m_searches.at(i);
+			return qMin(search.size(),1000); // maximum search results limited
 		} else return 0;
 	}
 }
 QModelIndex SearchResultModel::index(int row, int column, const QModelIndex &parent)
-	const
+const
 {
 	if(parent.isValid()){
 		int i=(parent.internalId()&0xFFFF0000)+(1<<15)+row;
@@ -57,7 +57,7 @@ QModelIndex SearchResultModel::index(int row, int column, const QModelIndex &par
 }
 
 QModelIndex SearchResultModel::parent(const QModelIndex &index)
-		const
+const
 {
 	quint32 i=index.internalId();
 	if((i&(1<<15))>0){
@@ -70,71 +70,71 @@ QVariant SearchResultModel::data(const QModelIndex &index, int role) const {
 	//if (index.row() >= log.count() || index.row() < 0) return QVariant();
 	if (role == Qt::ToolTipRole) return tr("Click to jump to the line");
 	if (role != Qt::DisplayRole) return QVariant();
-        QList<QDocumentLineHandle *> search;
-        QDocumentLineHandle *c;
+	QList<QDocumentLineHandle *> search;
+	QDocumentLineHandle *c;
 	int i=index.internalId();
 	if(i&(1<<15)){
 		i=(i>>16)-1;
 		search=m_searches.at(i);
-                c=search.value(index.row(),0);
-                if(c){
-                        QDocumentLine ln(c);
-                        QString temp=prepareResulText(ln.text());
-                        return QString("Line %1: ").arg(ln.lineNumber()+1)+temp;
+		c=search.value(index.row(),0);
+		if(c){
+			QDocumentLine ln(c);
+			QString temp=prepareResulText(ln.text());
+			return QString("Line %1: ").arg(ln.lineNumber()+1)+temp;
 		}else{
 			return "";
 		}
 	} else {
 		i=(i>>16)-1;
 		search=m_searches.at(i);
-                return index.row()<m_files.size() ? m_files.at(index.row())+QString(" (%1)").arg(search.size()) : "";
+		return index.row()<m_files.size() ? m_files.at(index.row())+QString(" (%1)").arg(search.size()) : "";
 	}
 }
 
 
 void SearchResultModel::setSearchExpression(const QString &exp,const bool isCaseSensitive,const bool isWord,const bool isRegExp){
-    mExpression=exp;
-    mIsCaseSensitive=isCaseSensitive;
-    mIsWord=isWord;
-    mIsRegExp=isRegExp;
+	mExpression=exp;
+	mIsCaseSensitive=isCaseSensitive;
+	mIsWord=isWord;
+	mIsRegExp=isRegExp;
 }
 
 QString SearchResultModel::prepareResulText(QString text) const{
-    QString result;
-    QList<QPair<int,int> > placements=getSearchResults(text);
-    int second;
-    if(placements.size()>0){
-        second=0;
-    } else return text;
-    for(int i=0;i<placements.size();i++){
-        int first=placements.at(i).first;
-        result.append(text.mid(second,first-second)); // add normal text
-        second=placements.at(i).second;
-        result.append("|"+text.mid(first,second-first)+"|"); // add highlighted text
-    }
-    result.append(text.mid(placements.last().second));
-    return result;
+	QString result;
+	QList<QPair<int,int> > placements=getSearchResults(text);
+	int second;
+	if(placements.size()>0){
+		second=0;
+	} else return text;
+	for(int i=0;i<placements.size();i++){
+		int first=placements.at(i).first;
+		result.append(text.mid(second,first-second)); // add normal text
+		second=placements.at(i).second;
+		result.append("|"+text.mid(first,second-first)+"|"); // add highlighted text
+	}
+	result.append(text.mid(placements.last().second));
+	return result;
 }
 
 QList<QPair<int,int> > SearchResultModel::getSearchResults(const QString &text) const{
-    if(mExpression.isEmpty()) return QList<QPair<int,int> >();
-
-    QRegExp m_regexp=generateRegExp(mExpression,mIsCaseSensitive,mIsWord,mIsRegExp);
-
-    int i=0;
-    QList<QPair<int,int> > result;
-    while(i<text.length() && i>-1){
-        i=m_regexp.indexIn(text,i);
-        if(i>-1){
-            if(!result.isEmpty() && result.last().second>i){
-                result.last().second=m_regexp.matchedLength()+i;
-            } else
-                result << qMakePair(i,m_regexp.matchedLength()+i);
-
-            i++;
-        }
-    }
-    return result;
+	if(mExpression.isEmpty()) return QList<QPair<int,int> >();
+	
+	QRegExp m_regexp=generateRegExp(mExpression,mIsCaseSensitive,mIsWord,mIsRegExp);
+	
+	int i=0;
+	QList<QPair<int,int> > result;
+	while(i<text.length() && i>-1){
+		i=m_regexp.indexIn(text,i);
+		if(i>-1){
+			if(!result.isEmpty() && result.last().second>i){
+				result.last().second=m_regexp.matchedLength()+i;
+			} else
+				result << qMakePair(i,m_regexp.matchedLength()+i);
+			
+			i++;
+		}
+	}
+	return result;
 }
 
 
@@ -145,15 +145,15 @@ QString SearchResultModel::getFilename(const QModelIndex &index){
 }
 int SearchResultModel::getLineNumber(const QModelIndex &index){
 	int i=index.internalId();
-        QList<QDocumentLineHandle*> search;
-        QDocumentLineHandle* c;
+	QList<QDocumentLineHandle*> search;
+	QDocumentLineHandle* c;
 	if(i&(1<<15)){
 		i=(i>>16)-1;
 		search=m_searches.at(i);
-                c=search.value(index.row(),0);
-                if(c){
-                        QDocumentLine ln(c);
-                        return ln.lineNumber();
+		c=search.value(index.row(),0);
+		if(c){
+			QDocumentLine ln(c);
+			return ln.lineNumber();
 		}else{
 			return -1;
 		}
@@ -172,16 +172,16 @@ QVariant SearchResultModel::headerData(int section, Qt::Orientation orientation,
 }
 
 int SearchResultModel::getNextSearchResultColumn(QString text,int col){
-    QRegExp m_regexp=generateRegExp(mExpression,mIsCaseSensitive,mIsWord,mIsRegExp);
-
-    int i=0;
-    int i_old=0;
-    while(i<=col && i>-1){
-        i=m_regexp.indexIn(text,i);
-        if(i>-1) {
-            i_old=i;
-            i++;
-        }
-    }
-    return i_old;
+	QRegExp m_regexp=generateRegExp(mExpression,mIsCaseSensitive,mIsWord,mIsRegExp);
+	
+	int i=0;
+	int i_old=0;
+	while(i<=col && i>-1){
+		i=m_regexp.indexIn(text,i);
+		if(i>-1) {
+			i_old=i;
+			i++;
+		}
+	}
+	return i_old;
 }
