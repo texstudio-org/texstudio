054ec5f0247f51fc7d1244cf00c51e0fb107efdf
Benito van der Zander <benito@benibela.de>
2012-03-14 22:43:26 +0000

    indentation
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@2650 92cce386-e96d-4106-809f-596c673a3fd8

--- searchresultmodel.h
+++ searchresultmodel.h
@@ -11,29 +11,29 @@ class SearchResultModel : public QAbstractItemModel
 public:
 	SearchResultModel(QObject *parent=0);
 	~SearchResultModel();
-
-        QVariant data(const QModelIndex &index, int role) const;
+	
+	QVariant data(const QModelIndex &index, int role) const;
 	QModelIndex index(int row, int column,const QModelIndex &parent = QModelIndex()) const;
 	QModelIndex parent(const QModelIndex &index) const;
 	int rowCount(const QModelIndex &parent = QModelIndex()) const;
 	int columnCount(const QModelIndex &parent = QModelIndex()) const;
 	QVariant headerData(int section, Qt::Orientation orientation, int role) const;
-
-        void addSearch(QList<QDocumentLineHandle *> newSearch,QString name="");
-        void removeSearch(QString name);
+	
+	void addSearch(QList<QDocumentLineHandle *> newSearch,QString name="");
+	void removeSearch(QString name);
 	void clear();
 	QString getFilename(const QModelIndex &index);
 	int getLineNumber(const QModelIndex &index);
-        void setSearchExpression(const QString &exp,const bool isCaseSensitive,const bool isWord,const bool isRegExp);
-        int getNextSearchResultColumn(QString text,int col);
+	void setSearchExpression(const QString &exp,const bool isCaseSensitive,const bool isWord,const bool isRegExp);
+	int getNextSearchResultColumn(QString text,int col);
 private:
-        QList<QPair<int,int> > getSearchResults(const QString &text) const;
-        QString prepareResulText(QString text) const;
-
-        QList< QList<QDocumentLineHandle *> > m_searches;
+	QList<QPair<int,int> > getSearchResults(const QString &text) const;
+	QString prepareResulText(QString text) const;
+	
+	QList< QList<QDocumentLineHandle *> > m_searches;
 	QStringList m_files;
-        QString mExpression;
-        bool mIsWord,mIsCaseSensitive,mIsRegExp;
+	QString mExpression;
+	bool mIsWord,mIsCaseSensitive,mIsRegExp;
 };
 
 #endif // SEARCHRESULTMODEL_H
