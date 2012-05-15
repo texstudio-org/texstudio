38e05369d4eee3cac56392ade0aa80cf55e65fe1
benibela <benito@benibela.de>
2012-05-15 20:31:41 +0000

    another approach to override assert
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@2839 92cce386-e96d-4106-809f-596c673a3fd8

--- modifiedQObject.h
+++ modifiedQObject.h
@@ -2,14 +2,39 @@
 #define MODIFIDIEDQOBJECT_H
 
 
+
+
+
 #ifndef QT_NO_DEBUG
+
+//defines copied and modified from qglobal.h (can't use Q_WS_WIN to check for windows without including qglobal.h first, but can't really include qglobal.h, because we want to change the assert definition there)
+#ifdef MY_WINDOWS
+#undef MY_WINDOWS
+#endif
+#if defined(__APPLE__) && (defined(__GNUC__) || defined(__xlC__) || defined(__xlc__))
+#elif defined(__SYMBIAN32__) || defined(SYMBIAN)
+#elif defined(__CYGWIN__)
+#elif defined(MSDOS) || defined(_MSDOS)
+#elif defined(__OS2__)
+#elif !defined(SAG_COM) && (defined(WIN64) || defined(_WIN64) || defined(__WIN64__))
+#define MY_WINDOWS
+#elif !defined(SAG_COM) && (defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__))
+#define MY_WINDOWS
+#elif defined(__MWERKS__) && defined(__INTEL__)
+#define MY_WINDOWS
+#endif
+
+#ifdef MY_WINDOWS
+#define Q_ASSERT(cond) ((!(cond)) ? txs_assert(#cond,__FILE__,__LINE__) : qt_noop())
+#undef MY_WINDOWS
+#else
 #define qt_assert txs_assert
-//void txs_assert(const char *assertion, const char *file, int line);
-//#define Q_ASSERT(cond) ((!(cond)) ? qt_assert(#cond,__FILE__,__LINE__) : qt_noop())
+#endif
+
+void txs_assert(const char *assertion, const char *file, int line);
 #endif
 
 #include <QObject>
 
-Q_CORE_EXPORT void txs_assert(const char *assertion, const char *file, int line);
 
 #endif
