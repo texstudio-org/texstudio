83e348ba5147c05de8b922d205bc9bdb2425bc1a
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2012-05-29 21:43:18 +0000

    add biblatex support to the bibliography menu
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@2896 92cce386-e96d-4106-809f-596c673a3fd8

--- bibtexdialog.h
+++ bibtexdialog.h
@@ -15,7 +15,7 @@ struct BibTeXType {
 			name(other.name), description(other.description), 
 			mandatoryFields(other.mandatoryFields), optionalFields(other.optionalFields){}
 	BibTeXType (const QString& d, const QString& n, const QStringList& mf, const QStringList& of):
-			name(n), description(d), mandatoryFields(mf), optionalFields(of){}		
+			name(n), description(d), mandatoryFields(mf), optionalFields(of){}
 };
 
 
@@ -23,14 +23,17 @@ class BibTeXDialog : public QDialog {
     Q_OBJECT
     Q_DISABLE_COPY(BibTeXDialog)
 public:
-    explicit BibTeXDialog(QWidget *parent = 0, const QStringList& fileList=QStringList(), int curFile=-1, const QString& defId="");
+	enum BibType {BIBTEX, BIBLATEX};
+
+	explicit BibTeXDialog(QWidget *parent = 0, const QStringList& fileList=QStringList(), int curFile=-1, const QString& defId="");
     virtual ~BibTeXDialog();
 
 	QString resultString;
 	int resultFileId; //-1 for new, 0..n for files from fileList, -2 for none
+	static void setBibType(BibType type);
 	static QString textToInsert(const BibTeXType& entry, bool keepOptionalFields, const QMap<QString,QString>& fields);
 	static QString textToInsert(const QString& entryName);
-	static QList<BibTeXType> getPossibleBibTeXTypes();
+	static QList<BibTeXType> getPossibleEntryTypes(BibType type);
 protected:
     virtual void changeEvent(QEvent *e);
 	virtual void accept();
@@ -38,8 +41,13 @@ private slots:
 	void typeSelectionChanged();
 private:
     Ui::BibTeXDialog *m_ui;
-	static void needTypes();
-	static QList<BibTeXType> types;
+	static void needEntryTypes();
+	static void generateBibtexEntryTypes(bool forceRecreate=false);
+	static void generateBiblatexEntryTypes(bool forceRecreate=false);
+	static QList<BibTeXType> *entryTypes;
+	static QList<BibTeXType> bibtexEntryTypes;
+	static QList<BibTeXType> biblatexEntryTypes;
+	static BibType bibType;
 };
 
 #endif // BIBTEXDIALOG_H
