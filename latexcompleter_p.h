72c4def411ea12c1c1d6e01ac60b4a9655dfd9f2
jsundermeyer <sunderme@gmx.de>
2012-07-25 20:47:37 +0000

    reduce displayed completion list when completing environs inside of mirror cursors
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@3051 92cce386-e96d-4106-809f-596c673a3fd8

--- latexcompleter_p.h
+++ latexcompleter_p.h
@@ -13,7 +13,7 @@ class CompletionListModel : public QAbstractListModel {
 	Q_OBJECT
 
 public:
-	CompletionListModel(QObject *parent = 0): QAbstractListModel(parent),mostUsedUpdated(false),mCanFetchMore(false),mLastMU(0), mWordCount(0) {}
+    CompletionListModel(QObject *parent = 0): QAbstractListModel(parent),mostUsedUpdated(false),mCanFetchMore(false),mLastMU(0),mEnvMode(false), mWordCount(0) {}
 
 	int rowCount(const QModelIndex &parent = QModelIndex()) const;
 	QVariant data(const QModelIndex &index, int role)const;
@@ -23,6 +23,7 @@ public:
 	const QSet<QChar>& getAcceptedChars(){return acceptedChars;}
 	bool isNextCharPossible(const QChar &c); //does this character lead to a new possible word
 	void filterList(const QString &word,int mostUsed=-1,bool fetchMore=false);
+    void setEnvironMode(bool mode);
 	void setBaseWords(const QSet<QString> &newwords,CompletionType completionType);
 	void setBaseWords(const QList<CompletionWord> &newwords, CompletionType completionType);
 	void setAbbrevWords(const QList<CompletionWord> &newwords);
@@ -46,6 +47,8 @@ private:
 	QString mLastWord;
 	int mLastMU;
 	CompletionWord mLastWordInList;
+
+    bool mEnvMode;
 
 	int mWordCount;
 
