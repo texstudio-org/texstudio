ed29deaec4fa0ec2665dd7dab4e6d58ea7527e58
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2012-11-09 01:22:44 +0000

    started refactoring: Move management of editors from texmaker.cpp to txstabwidget.cpp
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@3435 92cce386-e96d-4106-809f-596c673a3fd8

--- txstabwidget.cpp
+++ txstabwidget.cpp
@@ -1,9 +1,13 @@
 #include "txstabwidget.h"
+#include "latexeditorview.h"
 #include "smallUsefulFunctions.h"
 
 TxsTabWidget::TxsTabWidget(QWidget *parent) :
 		QTabWidget(parent)
 {
+	setFocusPolicy(Qt::ClickFocus);
+	setContextMenuPolicy(Qt::PreventContextMenu);
+
 	QTabBar *tb = new QTabBar();
 	tb->setContextMenuPolicy(Qt::CustomContextMenu);
 	connect(tb, SIGNAL(customContextMenuRequested(QPoint)), this, SIGNAL(tabBarContextMenuRequested(QPoint)));
@@ -28,3 +32,42 @@ void TxsTabWidget::moveTab(int from,int to){
 	else if (to > from && cur >= from && cur < to) 
 		setCurrentIndex(cur+1);
 }
+
+QList<LatexEditorView *> TxsTabWidget::editors() const {
+	QList<LatexEditorView *> list;
+	for (int i=0; i<count(); i++) {
+		LatexEditorView *edView = qobject_cast<LatexEditorView *>(widget(i));
+		Q_ASSERT(edView); // there should only be editors as tabs
+		list.append(edView);
+	}
+	return list;
+}
+
+LatexEditorView *TxsTabWidget::currentEditorView() const {
+	return qobject_cast<LatexEditorView *>(currentWidget());
+}
+
+void TxsTabWidget::setCurrentEditor(LatexEditorView *edView) {
+	if (currentWidget() == edView)
+		return;
+
+	REQUIRE(indexOf(edView)>=0);
+	setCurrentWidget(edView);
+}
+
+
+void TxsTabWidget::gotoNextDocument() {
+	if (count() <= 1) return;
+	int cPage = currentIndex() + 1;
+	if (cPage >= count()) setCurrentIndex(0);
+	else setCurrentIndex(cPage);
+}
+
+void TxsTabWidget::gotoPrevDocument() {
+	if (count() <= 1) return;
+	int cPage = currentIndex() - 1;
+	if (cPage < 0) setCurrentIndex(count() - 1);
+	else setCurrentIndex(cPage);
+}
+
+
