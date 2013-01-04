56b4ee66d709e150ee725a488e06e53eb3842cbb
t_hoffmann <thoffmann@texstudio.org>
2013-01-04 17:08:49 +0000

    commit missing changes to smallUsefulFunctions.h/cpp due to 3628
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@3630 92cce386-e96d-4106-809f-596c673a3fd8

--- smallUsefulFunctions.h
+++ smallUsefulFunctions.h
@@ -13,13 +13,12 @@
 
 #include "mostQtHeaders.h"
 
+// subgroups of utility functions:
+#include "utilsUI.h"
+#include "utilsSystem.h"
+#include "utilsVersion.h"
+
 //#inlcude "latexcompleter_config.h"
-
-#define TEXSTUDIO "TeXstudio"
-#define TXSVERSION "2.5.1"
-#define TXSVERSION_NUMERIC 0x020501
-
-extern const char* TEXSTUDIO_SVN_VERSION;
 
 struct CommandArgument {
 	bool isOptional;
@@ -28,51 +27,15 @@ struct CommandArgument {
 };
 Q_DECLARE_METATYPE( CommandArgument )
 
-int getSimplifiedSVNVersion(QString svnVersion = TEXSTUDIO_SVN_VERSION);
-
-bool txsConfirm(const QString &message);
-bool txsConfirmWarning(const QString &message);
-QMessageBox::StandardButton txsConfirmWarning(const QString &message, QMessageBox::StandardButtons buttons);
-void txsInformation(const QString &message);
-void txsWarning(const QString& message);
-void txsWarning(const QString &message, bool &noWarnAgain);
-void txsCritical(const QString& message);
-
 //void txs_assert(const char *assertion, const char *file, int line);
 
-bool getDiskFreeSpace(const QString &path, quint64 &freeBytes);
-QString getUserName();
 
 class LatexCompleterConfig;
 
-#define REQUIRE(x)  do { Q_ASSERT((x)); if (!(x)) return; } while (0)
-#define REQUIRE_RET(x,e) do { Q_ASSERT((x)); if (!(x)) return (e); } while (0)
 
 typedef QString (QObject::*StringToStringCallback)(const QString&) ;
 
 QString getCommonEOW();
-
-QStringList findResourceFiles(const QString& dirName, const QString& filter, QStringList additionalPreferredPaths = QStringList());
-//returns the real name of a resource file
-QString findResourceFile(const QString& fileName, bool allowOverride = false, QStringList additionalPreferredPaths = QStringList(), QStringList additionalFallbackPaths = QStringList());
-
-extern bool modernStyle;
-extern bool useSystemTheme;
-QString getRealIconFile(const QString& icon);
-QIcon getRealIcon(const QString& icon);
-
-//returns if the file is writable (QFileInfo.isWritable works in different ways on Windows and Linux)
-bool isFileRealWritable(const QString& filename);
-//returns if the file exists and is writable
-bool isExistingFileRealWritable(const QString& filename);
-//adds QDir::separator() if the path end not with it
-QString ensureTrailingDirSeparator(const QString& dirPath);
-// replaces "somdir/file.ext" to "somedir/file.newext"
-QString replaceFileExtension(const QString& filename, const QString& newExtension, bool appendIfNoExt=false);
-
-
-//returns kde version 0,3,4
-int x11desktop_env();
 
 
 //removes special latex characters
@@ -117,27 +80,14 @@ QStringList regExpFindAllMatches(const QString& searchIn, const QRegExp& regexp,
 // add Environment to QNFA DOM
 void addEnvironmentToDom(QDomDocument &doc,const QString& EnvironName,const QString& EnvironMode);
 
-//setup toolbutton as substitute for const combobox
-QToolButton* createComboToolButton(QWidget *parent,const QStringList& list, int height,const QObject * receiver, const char * member,QString defaultElem="",QToolButton *combo=0);
-//find the tool button which contains a given action
-QToolButton* comboToolButtonFromAction(QAction* action);
-
-QToolButton* createToolButtonForAction(QAction* action);
 
 
-//check if the run-time qt version is higher than the given version (e.g. 4,3)
-bool hasAtLeastQt(int major, int minor);
-
-QString getRelativeBaseNameToPath(const QString & file, QString basepath, bool baseFile=false, bool keepSuffix=false);
-QString getPathfromFilename(const QString &compFile);
 
 bool minimalJsonParse(const QString &text, QHash<QString, QString> &map);
 // generates a string in the format "id"  : "val", where the colon is at least a position minIdWidth+2 (filling with spaces)
 QString formatJsonStringParam(const QString &id, const QString &val, int minIdWidth = 0);
 QString enquoteStr(const QString &s);
 QString dequoteStr(const QString &s);
-
-bool connectUnique(const QObject * sender, const char * signal, const QObject * receiver, const char * method);
 
 QTextCodec * guessEncodingBasic(const QByteArray& data, int * outSure);
 
@@ -146,7 +96,7 @@ enum {
 	MIB_UTF8 = 106,
 	MIB_UTF16BE = 1013,
 	MIB_UTF16LE = 1014
-	
+
 };
 
 QString getImageAsText(const QPixmap &AImage);
@@ -269,31 +219,5 @@ public:
 };
 
 LatexPackage loadCwlFile(const QString fileName,LatexCompleterConfig *config=0);
-
-#if QT_VERSION >= 0x040700
-#define LIST_RESERVE(list, count) list.reserve(count)
-#else
-#define LIST_RESERVE(list, count) 
-#endif
-
-
-class ThreadBreaker : public QThread
-{
-public:
-    static void sleep(int s);
-	static void msleep(unsigned long ms);
-    static void forceTerminate(QThread* t = 0);
-};
-
-class SafeThread: public QThread
-{
-	Q_OBJECT
-public:
-	SafeThread();
-	SafeThread(QObject* parent);
-	void wait(unsigned long time = 60000);
-	bool crashed;
-};
-
 
 #endif
