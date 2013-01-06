52bbe9e520b2e688d440e7470723f46760c4aa09
t_hoffmann <thoffmann@texstudio.org>
2013-01-06 15:44:53 +0000

    fix document dirty after loading if it has a valid % !TeX encoding comment (bug 669)
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@3657 92cce386-e96d-4106-809f-596c673a3fd8

--- latexdocument.cpp
+++ latexdocument.cpp
@@ -2034,44 +2034,44 @@ bool LatexDocument::splitMagicComment(const QString &comment, QString &name, QSt
   Formats the StructureEntry and modifies the document according to the MagicComment contents
   */
 void LatexDocument::parseMagicComment(const QString &name, const QString &val, StructureEntry* se) {
-    if (name.isEmpty()) {
-        se->tooltip = QString();
-        se->valid = false;
-    }
+	if (name.isEmpty()) {
+		se->tooltip = QString();
+		se->valid = false;
+	}
 
-    if (name.toLower() == "spellcheck") {
-        QString lang=val;
-        lang.replace("-", "_"); // QLocale expects "_". This is to stay compatible with texworks which uses "-"
-        mSpellingLanguage = QLocale(lang);
-        if (mSpellingLanguage.language() == QLocale::C) {
-            se->tooltip = tr("Invalid language format");
-            return;
-        }
-        emit spellingLanguageChanged(mSpellingLanguage);
-
-        se->valid = true;
-        // TODO: set master document
-    } else if ((name.toLower() == "texroot")||(name.toLower() == "root")){
-        QString fname=findFileName(val);
-        LatexDocument* dc=parent->findDocumentFromName(fname);
-        if(dc)	setMasterDocument(dc);
-        else {
-            parent->addDocToLoad(fname);
-        }
-        se->valid = true;
-    } else if (name.toLower() == "encoding") {
-        QTextCodec *codec = QTextCodec::codecForName(val.toAscii());
-        if (!codec) {
-            se->tooltip = tr("Invalid codec");
-            return;
-        }
-        setCodec(codec);
-        se->valid = true;
-    } else {
-        se->tooltip = tr("Unknown magic comment");
-        return;
-    }
-    se->valid = true;
+	if (name.toLower() == "spellcheck") {
+		QString lang=val;
+		lang.replace("-", "_"); // QLocale expects "_". This is to stay compatible with texworks which uses "-"
+		mSpellingLanguage = QLocale(lang);
+		if (mSpellingLanguage.language() == QLocale::C) {
+			se->tooltip = tr("Invalid language format");
+			return;
+		}
+		emit spellingLanguageChanged(mSpellingLanguage);
+		se->valid = true;
+	} else if ((name.toLower() == "texroot")||(name.toLower() == "root")){
+		QString fname=findFileName(val);
+		LatexDocument* dc=parent->findDocumentFromName(fname);
+		if(dc)	setMasterDocument(dc);
+		else {
+			parent->addDocToLoad(fname);
+		}
+		se->valid = true;
+	} else if (name.toLower() == "encoding") {
+		bool hasUndo = canUndo();
+		QTextCodec *codec = QTextCodec::codecForName(val.toAscii());
+		if (!codec) {
+			se->tooltip = tr("Invalid codec");
+			return;
+		}
+		setCodec(codec);
+		if (!hasUndo) clearUndo(); // changing the codec pushes an entry to the undo/redo stack we don't want this for the first parsing at loading
+		se->valid = true;
+	} else {
+		se->tooltip = tr("Unknown magic comment");
+		return;
+	}
+	se->valid = true;
 }
 
 
