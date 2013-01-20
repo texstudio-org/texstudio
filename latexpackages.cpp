33a13997b0aefc202e039e1f9fdc0a4424d85246
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2013-01-20 21:41:41 +0000

    revert unwanted changes in commit 3716
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@3717 92cce386-e96d-4106-809f-596c673a3fd8

--- latexpackages.cpp
+++ latexpackages.cpp
@@ -7,7 +7,6 @@ LatexPackages::LatexPackages() :
 	QObject(0), m_dataSource(None)
 {
 	loadStaticPackageList(":/utilities/packageList");
-	queryPackageList();
 }
 
 LatexPackages *LatexPackages::instance() {
@@ -21,14 +20,6 @@ LatexPackages *LatexPackages::instance() {
 
 LatexPackages::DataSource LatexPackages::dataSource() {
 	return m_dataSource;
-}
-
-bool LatexPackages::packageExists(const QString& name) {
-	return packages.keys().contains(name);
-}
-
-QString LatexPackages::shortDescription(const QString& name) {
-	return packages[name].shortDescription;
 }
 
 bool LatexPackages::loadStaticPackageList(const QString &file) {
@@ -53,44 +44,10 @@ bool LatexPackages::loadStaticPackageList(const QString &file) {
 	return true;
 }
 
-bool LatexPackages::queryPackageList() {
-	QStringList args;
-	args << "--help";
-	QProcess *proc = new QProcess(this);
-	//proc->setProperty("package", package);
-	connect(proc, SIGNAL(readyReadStandardOutput()), SLOT(queryReadData()));
-	connect(proc, SIGNAL(readyReadStandardError()), SLOT(queryReadError()));
-	connect(proc, SIGNAL(finished(int)), SLOT(queryPackageListCompleted(int)));
-	connect(proc, SIGNAL(error(QProcess::ProcessError)), SLOT(queryError(QProcess::ProcessError)));
-	proc->start("C:/texlive/2012/bin/win32/tlmgr.bat", args);
-	qDebug() << "baz";
-	return true;
+bool LatexPackages::packageExists(const QString& name) {
+	return packages.keys().contains(name);
 }
 
-void LatexPackages::queryReadData() {
-	QProcess *proc = qobject_cast<QProcess *>(sender());
-	qDebug() << proc->readAllStandardOutput();
-	m_queryData += proc->readAllStandardOutput() + '\n';
-	qDebug() << "reading";
-}
-
-void LatexPackages::queryReadError() {
-	QProcess *proc = qobject_cast<QProcess *>(sender());
-	qDebug() << "error:" << proc->readAllStandardError();
-}
-
-void LatexPackages::queryPackageListCompleted(int exitCode) {
-	qDebug() << "foo";
-	qDebug() << "content" << m_queryData;
-	Q_UNUSED(exitCode);
-	QProcess *proc = qobject_cast<QProcess *>(sender());
-	if (!proc) return;
-	//QString package = proc->property("package").toString();
-	QString info = proc->readAll();
-	qDebug() << info << exitCode;
-	proc->deleteLater();
-}
-
-void LatexPackages::queryError(QProcess::ProcessError error) {
-	qDebug() << "error" << error;
+QString LatexPackages::shortDescription(const QString& name) {
+	return packages[name].shortDescription;
 }
