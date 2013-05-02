9cb6f6c99b29606c6641a3735620e7d0cbb4abac
t_hoffmann <thoffmann@texstudio.org>
2013-05-02 18:07:12 +0000

    Added following enviroments as math tables, so that table alignment can be employed: align*, split, multline, multline*, gather, gather*, flalign, flalign*, alignat, alignat*
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@3962 92cce386-e96d-4106-809f-596c673a3fd8

--- tablemanipulation.cpp
+++ tablemanipulation.cpp
@@ -7,7 +7,7 @@
 
 QStringList LatexTables::tabularNames = QStringList() << "tabular" << "array" << "longtable" << "supertabular" << "tabu" << "longtabu";
 QStringList LatexTables::tabularNamesWithOneOption = QStringList() << "tabular*" << "tabularx";
-QStringList LatexTables::mathTables = QStringList() << "align" << "array" << "matrix" << "bmatrix" << "pmatrix" << "vmatrix" << "Vmatrix";
+QStringList LatexTables::mathTables = QStringList() << "align" << "align*" << "array" << "matrix" << "bmatrix" << "pmatrix" << "vmatrix" << "Vmatrix" << "split" << "multline" << "multline*" << "gather" << "gather*" << "flalign" << "flalign*" << "alignat" << "alignat*";
 
 void LatexTables::addRow(QDocumentCursor &c,const int numberOfColumns ){
 	QDocumentCursor cur(c);
