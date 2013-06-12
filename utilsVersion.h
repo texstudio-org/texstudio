e18cb581230f32069f509a23f5553c7eff7fe40c
Benito van der Zander <benito@benibela.de>
2013-06-12 10:13:38 +0000

    update version number => 2.6.0
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@4046 92cce386-e96d-4106-809f-596c673a3fd8

--- utilsVersion.h
+++ utilsVersion.h
@@ -2,7 +2,7 @@
 #define UTILSVERSION_H
 
 #define TEXSTUDIO "TeXstudio"
-#define TXSVERSION "2.6.0 beta"
+#define TXSVERSION "2.6.0"
 #define TXSVERSION_NUMERIC 0x020600
 
 extern const char* TEXSTUDIO_SVN_VERSION;
