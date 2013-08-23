076af12626220632eb8182ab28c5d4428daeb8f7
jsundermeyer <14033169+sunderme@users.noreply.github.com>
2013-08-23 21:28:02 +0000

    remove callgrind commands
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@4179 92cce386-e96d-4106-809f-596c673a3fd8

--- texmaker.cpp
+++ texmaker.cpp
@@ -11,7 +11,7 @@
  ***************************************************************************/
 //#include <stdlib.h>
 
-#include "/usr/include/valgrind/callgrind.h"
+//#include "/usr/include/valgrind/callgrind.h"
 
 #include "texmaker.h"
 #include "latexeditorview.h"
@@ -2544,7 +2544,7 @@ void Texmaker::fileSaveSession() {
 }
 
 void Texmaker::restoreSession(const Session &s, bool showProgress) {
-CALLGRIND_START_INSTRUMENTATION;
+//CALLGRIND_START_INSTRUMENTATION;
 	fileCloseAll();
 
 	cursorHistory->setInsertionEnabled(false);
@@ -2609,7 +2609,7 @@ CALLGRIND_START_INSTRUMENTATION;
     // update completer
     if(currentEditorView())
         updateCompleter(currentEditorView());
-CALLGRIND_STOP_INSTRUMENTATION;
+//CALLGRIND_STOP_INSTRUMENTATION;
 }
 
 Session Texmaker::getCurrentSession() {
