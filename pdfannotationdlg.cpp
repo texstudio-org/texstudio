600e85e1d94eaf95c61c2533650c03cf5c2865f0
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2013-08-24 16:50:42 +0000

    fix typo
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@4182 92cce386-e96d-4106-809f-596c673a3fd8

--- pdfannotationdlg.cpp
+++ pdfannotationdlg.cpp
@@ -6,14 +6,6 @@ PDFAnnotationDlg::PDFAnnotationDlg(Poppler::Annotation *annot, QWidget *parent) 
 	QDialog(parent),
 	ui(new Ui::PDFAnnotationDlg)
 {
-
-
-
-	enum SubType { AText = 1, ALine = 2, AGeom = 3, AHighlight = 4, AStamp = 5,
-				   AInk = 6, ALink = 7, ACaret = 8, AFileAttachment = 9, ASound = 10,
-				   AMovie = 11, A_BASE = 0 };
-
-
 	ui->setupUi(this);
 	ui->lbUser->setText(annot->author());
 	ui->lbModDate->setText(annot->modificationDate().toString());
@@ -31,7 +23,7 @@ QString PDFAnnotationDlg::subType(Poppler::Annotation::SubType subtype)
 	switch (subtype) {
 	case Poppler::Annotation::AText: return tr("Text");
 	case Poppler::Annotation::ALine: return tr("Line");
-	case Poppler::Annotation::AGeom: return tr("Gemetric");
+	case Poppler::Annotation::AGeom: return tr("Geometric");
 	case Poppler::Annotation::AHighlight: return tr("Highlight");
 	case Poppler::Annotation::AStamp: return tr("Stamp");
 	case Poppler::Annotation::AInk: return tr("Ink");
