9ba68d8a902c0dc7ef38da603998db0a1504e456
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2013-11-01 18:37:48 +0100

    fix: hg_revision.bat gave tip of repository instead of current revision

--- hg_revision.bat
+++ hg_revision.bat
@@ -1,5 +1,11 @@
 @echo off
 cd %0\.. 
-for /f "tokens=2" %%g in ('hg log -l 1 ^| findstr "changeset"') DO @set revision=%%g
-echo const char * TEXSTUDIO_HG_REVISION = "%revision%";  > hg_revision.cpp
+
+hg id -n > hg_temp.txt
+set /p local_revision= < hg_temp.txt
+hg id -i > hg_temp.txt
+set /p global_revision= < hg_temp.txt
+del hg_temp.txt
+
+echo const char * TEXSTUDIO_HG_REVISION = "%local_revision%:%global_revision%";  > hg_revision.cpp
 %1 -c hg_revision.cpp -o %2/hg_revision.o
