5277bf2e0e5e90114d4b06717fcf5c1682aaed79
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2014-01-27 19:45:21 +0100

    fix space allocation for structure view can't be resized on OSX https://sourceforge.net/p/texstudio/bugs/768/

--- txstabwidget.cpp
+++ txstabwidget.cpp
@@ -10,6 +10,7 @@ TxsTabWidget::TxsTabWidget(QWidget *parent) :
 
 	ChangeAwareTabBar *tb = new ChangeAwareTabBar();
 	tb->setContextMenuPolicy(Qt::CustomContextMenu);
+	tb->setUsesScrollButtons(true);
 	connect(tb, SIGNAL(customContextMenuRequested(QPoint)), this, SIGNAL(tabBarContextMenuRequested(QPoint)));
 	connect(tb, SIGNAL(currentTabAboutToChange(int,int)), this, SLOT(currentTabAboutToChange(int,int)));
 	setTabBar(tb);
