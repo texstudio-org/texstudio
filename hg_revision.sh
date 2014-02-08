157646bcfc43e14175e1ee8ffbc8f34b4e4eb7ec
Benito van der Zander <benito@benibela.de>
2014-02-08 01:27:42 +0100

    include hg rev hash in nix about dialog

--- hg_revision.sh
+++ hg_revision.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 version=$(hg identify -n)
-echo "const char * TEXSTUDIO_HG_REVISION = \"$version\";" > hg_revision.cpp
+revision=$(hg identify -i)
+echo "const char * TEXSTUDIO_HG_REVISION = \"$version:$revision\";" > hg_revision.cpp
 $1 -c hg_revision.cpp -o $2/hg_revision.o
