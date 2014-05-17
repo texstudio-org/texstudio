8dd17962b59ea9d3e038d8f7f7df03e86674b2c1
Tim Hoffmann <thoffmann@texstudio.org>
2014-05-17 15:44:09 +0200

    default shortcut for "Glossary": F10

--- texmaker.cpp
+++ texmaker.cpp
@@ -808,7 +808,7 @@ void Texmaker::setupMenus() {
 	newManagedAction(menu, "view",tr("&View"), SLOT(commandFromAction()), Qt::Key_F7,"viewer")->setData(BuildManager::CMD_VIEW);
 	newManagedAction(menu, "bibtex",tr("&Bibliography"), SLOT(commandFromAction()), Qt::Key_F11)->setData(BuildManager::CMD_BIBLIOGRAPHY);
 	newManagedAction(menu, "index",tr("&Index"), SLOT(commandFromAction()), Qt::Key_F12)->setData(BuildManager::CMD_INDEX);
-	newManagedAction(menu, "glossary",tr("&Glossary"), SLOT(commandFromAction()))->setData(BuildManager::CMD_GLOSSARY);
+	newManagedAction(menu, "glossary",tr("&Glossary"), SLOT(commandFromAction()), Qt::Key_F10)->setData(BuildManager::CMD_GLOSSARY);
 
 	menu->addSeparator();
 	submenu=newManagedMenu(menu, "commands",tr("&Commands", "menu"));
