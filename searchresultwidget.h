57ceec7bea6154311d0e22dc904c5b2f73727e2d
Tim Hoffmann <thoffmann@texstudio.org>
2015-05-24 20:03:58 +0200

    code cleanup (style)

--- searchresultwidget.h
+++ searchresultwidget.h
@@ -13,49 +13,48 @@ class SearchResultWidget : public QWidget
 public:
 	explicit SearchResultWidget(QWidget *parent = 0);
 	~SearchResultWidget();
-	
-	void setSearchExpression(QString exp,bool isCase,bool isWord,bool isRegExp);
-    void setSearchExpression(QString exp,QString replaceText,bool isCase,bool isWord,bool isRegExp);
+
+	void setSearchExpression(QString exp, bool isCase, bool isWord, bool isRegExp);
+	void setSearchExpression(QString exp, QString replaceText, bool isCase, bool isWord, bool isRegExp);
 	QString searchExpression() const;
-    int getSearchScope() const;
-	int getNextSearchResultColumn(QString text,int col);
-	
+	int getSearchScope() const;
+	int getNextSearchResultColumn(QString text, int col);
+
 signals:
-	void jumpToSearch(QDocument* doc,int lineNumber);
+	void jumpToSearch(QDocument *doc, int lineNumber);
 	void updateTheSearch(int);
-	
+
 public slots:
-	void addSearch(QList<QDocumentLineHandle *> search, QDocument* doc);
+	void addSearch(QList<QDocumentLineHandle *> search, QDocument *doc);
 	void clearSearch();
-    void setSearchEditors(QList<LatexDocument *> docs){
-        mDocs=docs;
-    }
+	void setSearchEditors(QList<LatexDocument *> docs) {
+		mDocs = docs;
+	}
 
 private slots:
-	void clickedSearchResult(const QModelIndex& index);
-    void updateSearch();
-    void replaceAll();
-    void replaceTextChanged(QString text);
+	void clickedSearchResult(const QModelIndex &index);
+	void updateSearch();
+	void replaceAll();
+	void replaceTextChanged(QString text);
 
 private:
 	QLabel *searchTextLabel;
-    QLineEdit *replaceTextEdit;
-    QComboBox *searchScopeBox;
+	QLineEdit *replaceTextEdit;
+	QComboBox *searchScopeBox;
 	SearchResultModel *searchResultModel;
 	QTreeView *searchTree;
-	QList<LatexDocument*> mDocs;
-	
+	QList<LatexDocument *> mDocs;
+
 	void retranslateUi();
 };
 
 class SearchTreeDelegate: public QItemDelegate {
 	Q_OBJECT
 public:
-	SearchTreeDelegate(QObject * parent = 0 );
+	SearchTreeDelegate(QObject *parent = 0);
 protected:
-	void paint( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const;
-	//void drawDisplay( QPainter* painter, const QStyleOptionViewItem& option, const QRect& rect, const QString& text ) const;
-	QSize sizeHint(const QStyleOptionViewItem &option,const QModelIndex &index) const;
+	void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+	QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
 };
 
 
