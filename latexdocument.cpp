9a0cb4bff3ad46e8c127de1028e06fcb428f3478
Jan Sundermeyer <jsundermeyer@sf.net>
2015-06-01 21:45:58 +0200

    fix multiline tokens

--- latexdocument.cpp
+++ latexdocument.cpp
@@ -1040,7 +1040,7 @@ bool LatexDocument::patchStructure(int linenr, int count,bool recheck) {
         parent->addDocToLoad(fname);
     }
     //qDebug()<<"leave"<< QTime::currentTime().toString("HH:mm:ss:zzz");
-    if(reRunSuggested)
+    if(reRunSuggested && !recheck)
         patchStructure(0,-1,true); // expensive solution for handling changed packages (and hence command definitions)
     return reRunSuggested;
 }
