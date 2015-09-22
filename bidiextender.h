0aa1888ae57a7c88201545be02037664c52828bf
benibela <benito@benibela.de>
2015-09-22 21:47:48 +0200

    ignore keyboard layout change when it the current language context is ambigious

--- bidiextender.h
+++ bidiextender.h
@@ -1,6 +1,8 @@
 #ifndef BIDIEXTENDER_H
 #define BIDIEXTENDER_H
 
-void setInputLanguage(bool englishLikeLTR);
+enum InputLanguage { IL_UNCERTAIN, IL_LTR, IL_RTL };
+
+void setInputLanguage(InputLanguage lang);
 
 #endif // BIDIEXTENDER_H
