721b3a6b58bb10ad65b0e3cffef250e4536bf64e
jsundermeyer <jsundermeyer@sf.net>
2015-10-03 19:41:41 +0200

    updateCompletion fixed on mac

--- completion/updateCompletionQRC.sh
+++ completion/updateCompletionQRC.sh
@@ -5,7 +5,7 @@ echo "Generate completion.qrc"
 rm ../completion.qrc
 echo "<RCC>">../completion.qrc
 echo "<qresource prefix=\"/\">">>../completion.qrc
-ls -1 *.cwl|xargs -i echo "<file>"completion/{}"</file>" >> ../completion.qrc
-ls -1 *.dat|xargs -i echo "<file>"completion/{}"</file>" >> ../completion.qrc
+ls -1 *.cwl|xargs -I '{}' echo "<file>"completion/{}"</file>" >> ../completion.qrc
+ls -1 *.dat|xargs -I '{}' echo "<file>"completion/{}"</file>" >> ../completion.qrc
 echo "</qresource>">>../completion.qrc
 echo "</RCC>">>../completion.qrc
