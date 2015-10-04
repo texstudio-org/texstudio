6b3d835ca63cfae000b8c3cc80769142d3711391
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2015-10-04 19:50:58 +0200

    more granular selection of update notifications: stable / release candidate /development

--- utilsVersion.h
+++ utilsVersion.h
@@ -15,4 +15,32 @@ extern const char* TEXSTUDIO_HG_REVISION;
 #define COMPILED_DEBUG_OR_RELEASE "D"
 #endif
 
+#include "mostQtHeaders.h"
+
+int hgRevisionToInt(const char *);
+
+class Version {
+
+public:
+	enum VersionCompareResult {Invalid=-2, Lower=-1, Same=0, Higher=1};
+	static VersionCompareResult compareStringVersion(const QString &v1, const QString &v2);
+	static VersionCompareResult compareIntVersion(const QList<int> &v1, const QList<int> &v2);
+	static QList<int> parseVersionNumber(const QString &versionNumber);
+	static bool versionNumberIsValid(const QString &versionNumber);
+	
+	Version() : revision(0) {}
+	Version(QString number, int rev=0) : versionNumber(number), revision(rev) {}
+	static Version current();
+
+	QString platform;       // "win" or "mac" or "linux"
+	QString versionNumber;  // "2.10.2"
+	QString type;           // "stable", "release candidate" or "development"
+	int revision;           // 5310
+
+	bool operator > (const Version &other) const;
+	
+	bool isEmpty() const;
+	bool isValid() const;
+	
+};
 #endif // UTILSVERSION_H
