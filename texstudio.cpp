ded765f7e8b4b5091b612427a13305ccb923c1eb
Jan Sundermeyer <jsundermeyer@sf.net>
2015-10-23 16:01:53 +0200

    revert erroneous correction

--- texstudio.cpp
+++ texstudio.cpp
@@ -9389,7 +9389,7 @@ void Texstudio::updateTexQNFA()
 	documents.enablePatch(false);
 	foreach (LatexEditorView *edView, EditorTabs->editors()) {
 		QEditor *ed = edView->editor;
-		if (ed->languageDefinition() != oldLaTeX) {
+        if (ed->languageDefinition() == oldLaTeX) {
 			ed->setLanguageDefinition(newLaTeX);
 			ed->highlight();
 		}
