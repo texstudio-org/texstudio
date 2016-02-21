cd8085dbf8a96d3493486b51b978d33efa2c7a6d
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2016-02-22 00:18:17 +0100

    fix argument detection: optional arguments may appear after mandatory ones e.g. \newenvironment{env}[nargs][optargdefault]{begdefn}{enddefn}

--- smallUsefulFunctions.cpp
+++ smallUsefulFunctions.cpp
@@ -2749,9 +2749,6 @@ QString getArg(TokenList tl, QDocumentLineHandle *dlh, int argNumber, ArgumentLi
                     return result;
                 else
                     k++;
-            } else {
-                if (type == ArgumentList::Optional)
-                    return QString(); //optional argument can't follow mandatory one
             }
         }
         lineNr++;
