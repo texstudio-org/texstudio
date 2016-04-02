4ddd115d6af93afee2633ed196c13ffd9f02e5cf
Jan Sundermeyer <jsundermeyer@sf.net>
2016-04-02 17:09:12 +0200

    extend code documentation

--- syntaxcheck.h
+++ syntaxcheck.h
@@ -10,17 +10,19 @@
 #include <QQueue>
 
 class LatexDocument;
-
+/*!
+ * \brief store information on open environments
+ */
 class Environment
 {
 public:
-	Environment(): id(-1), excessCol(0), dlh(0), ticket(0) {}
+	Environment(): id(-1), excessCol(0), dlh(0), ticket(0) {} ///< constructor
 
-	QString name;
-    QString origName;
-	int id;
-	int excessCol;
-	QDocumentLineHandle *dlh;
+	QString name; ///< name of environment, partially an alias is used, e.g. math instead of '$'
+    QString origName; ///< original name of environment if alias is used, otherwise empty
+	int id; ///< mostly unused, contains the number of columns for tabular-environments
+	int excessCol; ///< number of unused tabular-columns if columns are strechted over several text lines
+	QDocumentLineHandle *dlh; ///< linehandle of starting line
 	int ticket;
 
 	bool operator ==(const Environment &env) const
@@ -42,34 +44,42 @@ class SyntaxCheck : public SafeThread
 	Q_OBJECT
 
 public:
+    /*!
+     * \brief type of error
+     */
 	enum ErrorType {
-		ERR_none,
-		ERR_unrecognizedCommand,
-		ERR_unrecognizedMathCommand,
-		ERR_unrecognizedTabularCommand,
-		ERR_TabularCommandOutsideTab,
-		ERR_MathCommandOutsideMath,
-		ERR_TabbingCommandOutside,
-		ERR_tooManyCols,
-		ERR_tooLittleCols,
-		ERR_missingEndOfLine,
-		ERR_closingUnopendEnv,
-		ERR_EnvNotClosed,
-		ERR_unrecognizedKey,
-		ERR_unrecognizedKeyValues
+		ERR_none, ///< no error
+		ERR_unrecognizedCommand, ///< command unknown
+		ERR_unrecognizedMathCommand, ///< unknown command for math environment
+		ERR_unrecognizedTabularCommand, ///< unknown command for tabular environment
+		ERR_TabularCommandOutsideTab, ///< tabular command outside tabular (e.g. \hline)
+		ERR_MathCommandOutsideMath, ///< math command outside of math env
+		ERR_TabbingCommandOutside, ///< tabbing command outside of tabbing env
+		ERR_tooManyCols, ///< tabular has more columns in line than in definition
+		ERR_tooLittleCols, ///< tabular has fewer columns in line than in definition
+		ERR_missingEndOfLine, ///< unused
+		ERR_closingUnopendEnv, ///< end{env} without corrresponding begin{env}
+		ERR_EnvNotClosed, ///< end{env} missing
+		ERR_unrecognizedKey, ///< in key/value argument, an unknown key is used
+		ERR_unrecognizedKeyValues ///< in key/value argument, an unknown value is used for a key
 	};
-
+    /*!
+     * \brief info which is queued for syntaxchecking
+     */
 	struct SyntaxLine {
-		StackEnvironment prevEnv;
-		TokenStack stack;
-		int ticket;
-		bool clearOverlay;
-		QDocumentLineHandle *dlh;
+		StackEnvironment prevEnv; ///< environmentstack at start of line
+		TokenStack stack; ///< tokenstack at start of line (open arguments)
+		int ticket; ///< ticket number
+		bool clearOverlay; ///< clear syntax overlay, sometimes not necessary as it was done somewhere else
+		QDocumentLineHandle *dlh; ///< linehandle
 	};
 
+    /*!
+     * \brief structure to describe an syntax error
+     */
 	struct Error {
-		QPair<int, int> range;
-		ErrorType type;
+		QPair<int, int> range; ///<  start,stop of error marker
+		ErrorType type; ///< type of error
 	};
 
 	typedef QList<Error > Ranges;
@@ -80,7 +90,7 @@ public:
 	void stop();
 	void setErrFormat(int errFormat);
 	QString getErrorAt(QDocumentLineHandle *dlh, int pos, StackEnvironment previous, TokenStack stack);
-	int verbatimFormat;
+	int verbatimFormat; ///< format number for verbatim text (LaTeX)
 	void waitForQueueProcess();
 	static int containsEnv(const LatexParser &parser, const QString &name, const StackEnvironment &envs, const int id = -1);
 	int topEnv(const QString &name, const StackEnvironment &envs, const int id = -1);
@@ -91,7 +101,7 @@ public:
 	void markUnclosedEnv(Environment env);
 
 signals:
-	void checkNextLine(QDocumentLineHandle *dlh, bool clearOverlay, int ticket);
+	void checkNextLine(QDocumentLineHandle *dlh, bool clearOverlay, int ticket); ///< enqueue next line for syntax checking as context has changed
 
 protected:
 	void run();
