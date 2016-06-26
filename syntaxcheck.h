0ecdc35da415348fa1e31fb229b1fabb44ea6a8b
jsundermeyer <14033169+sunderme@users.noreply.github.com>
2016-06-26 12:29:49 +0200

    fix nested double backslash in tabular/command add some doxygen comments in grammarcheck.cpp

--- syntaxcheck.h
+++ syntaxcheck.h
@@ -16,7 +16,7 @@ class LatexDocument;
 class Environment
 {
 public:
-	Environment(): id(-1), excessCol(0), dlh(0), ticket(0) {} ///< constructor
+    Environment(): id(-1), excessCol(0), dlh(0), ticket(0), level(0) {} ///< constructor
 
 	QString name; ///< name of environment, partially an alias is used, e.g. math instead of '$'
     QString origName; ///< original name of environment if alias is used, otherwise empty
@@ -24,14 +24,15 @@ public:
 	int excessCol; ///< number of unused tabular-columns if columns are strechted over several text lines
 	QDocumentLineHandle *dlh; ///< linehandle of starting line
 	int ticket;
+    int level; ///< command level (see tokens) in order to handle nested commands like \shortstack
 
 	bool operator ==(const Environment &env) const
 	{
-		return (name == env.name) && (id == env.id) && (excessCol == env.excessCol) && (origName == env.origName);
+        return (name == env.name) && (id == env.id) && (excessCol == env.excessCol) && (origName == env.origName) && (level == env.level);
 	}
 	bool operator !=(const Environment &env) const
 	{
-		return (name != env.name) || (id != env.id) || (excessCol != env.excessCol) || (origName != env.origName);
+        return (name != env.name) || (id != env.id) || (excessCol != env.excessCol) || (origName != env.origName) || (level != env.level);
 	}
 };
 
