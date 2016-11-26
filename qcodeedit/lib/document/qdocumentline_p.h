1d4a929b379421342d8a8880f522b227a480739c
Tim Hoffmann <thoffmann@texstudio.org>
2016-11-26 17:45:36 +0100

    improvements on obtaining QFormatRanges
    rename getFirstOverlayBetween/getFirstOverlayBetween to getFirstOverlay/getLastOverlay
    argument start, end are now optional
    default: start = 0, end = -1 (no limit)

--- qcodeedit/lib/document/qdocumentline_p.h
+++ qcodeedit/lib/document/qdocumentline_p.h
@@ -111,8 +111,8 @@ public:
 		bool hasOverlay(int id);
 		QList<QFormatRange> getOverlays(int preferredFormat) const;
 		QFormatRange getOverlayAt(int index, int preferredFormat) const;
-		QFormatRange getFirstOverlayBetween(int start, int end, int preferredFormat = -1) const;
-		QFormatRange getLastOverlayBetween(int start, int end, int preferredFormat = -1) const;
+		QFormatRange getFirstOverlay(int start = 0, int end = -1, int preferredFormat = -1) const;
+		QFormatRange getLastOverlay(int start = 0, int end = -1, int preferredFormat = -1) const;
 		
 		void shiftOverlays(int position, int offset);
 		
