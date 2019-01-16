0ccc725c7c89f250873289166b8f141c6ccb64fa
Jan Sundermeyer <sunderme@gmx.de>
2019-01-16 19:50:45 +0100

    use service for xvfb

--- travis-ci/configure.sh
+++ travis-ci/configure.sh
@@ -11,8 +11,8 @@ print_headline "Configuring for building for ${QT} on ${TRAVIS_OS_NAME}"
 
 if [ "${QT}" = "qt5" ]; then
 	qmake texstudio.pro CONFIG+=debug
-  	sh -e /etc/init.d/xvfb start
-	sleep 3
+  	##sh -e /etc/init.d/xvfb start
+	##sleep 3
 elif [ $QT = qt5NoPoppler ]; then
 	qmake texstudio.pro CONFIG+=debug NO_POPPLER_PREVIEW=1
 elif [ $QT = qt4 ]; then
