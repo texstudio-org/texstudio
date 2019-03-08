379074830be325d941789707078019254a928722
Jan Sundermeyer <sunderme@gmx.de>
2019-03-08 22:33:40 +0100

    replicate tw set-up

--- travis-ci/mxe/build-poppler-mxe.mk
+++ travis-ci/mxe/build-poppler-mxe.mk
@@ -4,7 +4,11 @@ poppler : $(poppler_FILE)
 	if [ "$(shell openssl dgst -sha256 '$^' 2>/dev/null)" != "SHA256($(poppler_FILE))= $(poppler_CHECKSUM)" ]; then echo "\nwrong checksum"; exit 1; fi
 	tar -xf $^
 	for F in poppler-*.patch; do echo "Applying $$F"; patch -d $(poppler_SUBDIR) -p1 < "$$F"; done
-	$(call poppler_BUILD,$(poppler_SUBDIR),poppler-test)
+	$(eval SOURCE_DIR := $(shell pwd)/$(poppler_SUBDIR))
+	$(eval BUILD_DIR := $(poppler_SUBDIR)/build)
+	$(eval TEST_FILE := $(shell pwd)/poppler-test.cxx)
+	mkdir $(BUILD_DIR)
+	$(call poppler_BUILD)
 
 $(poppler_FILE) :
 	wget --no-check-certificate -O "$(poppler_FILE)" "$(poppler_URL)"
--- travis-ci/mxe/poppler.mk
+++ travis-ci/mxe/poppler.mk
@@ -1,31 +1,30 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := poppler
-$(PKG)_WEBSITE  := https://poppler.freedesktop.org/
+$(PKG)_WEBSITE  := https://poppler.freedesktop.org
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 0.74.0
 $(PKG)_CHECKSUM := 92e09fd3302567fd36146b36bb707db43ce436e8841219025a82ea9fb0076b2f
 $(PKG)_SUBDIR   := poppler-$($(PKG)_VERSION)
 $(PKG)_FILE     := poppler-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := https://poppler.freedesktop.org/$($(PKG)_FILE)
-#$(PKG)_DEPS     := gcc cairo curl freetype glib jpeg lcms libpng qt tiff zlib
-$(PKG)_DEPS     :=  cc cairo curl freetype glib jpeg lcms libpng  libwebp openjpeg qtbase tiff zlib
+$(PKG)_URL      := https://poppler.freedesktop.org/poppler-$($(PKG)_VERSION).tar.xz
+$(PKG)_DEPS     := cc cairo curl freetype glib jpeg lcms libpng libwebp openjpeg qtbase tiff zlib
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://poppler.freedesktop.org/' | \
-    $(SED) -n 's,.*"poppler-\([0-9.]\+\)\.tar\.xz".*,\1,p' | \
-    head -1
+    $(call GET_LATEST_VERSION, https://poppler.freedesktop.org/releases.html, poppler-)
 endef
 
 define $(PKG)_BUILD
-    mkdir -p '$(1)/build'
-    cd '$(1)/build' && '$(TARGET)-cmake' .. -DENABLE_UNSTABLE_API_ABI_HEADERS=ON -DENABLE_TESTS=OFF -DENABLE_UTILS=OFF -DENABLE_LIBOPENJPEG='none'
-    $(MAKE) -C '$(1)/build' -j 1 $(MXE_DISABLE_CRUFT) HTML_DIR=
-    $(MAKE) -C '$(1)/build' -j 1 install $(MXE_DISABLE_CRUFT) HTML_DIR=
+    # build and install the library
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake \
+        -DENABLE_UNSTABLE_API_ABI_HEADERS=ON -DENABLE_TESTS=OFF -DENABLE_UTILS=OFF \
+        '$(SOURCE_DIR)'
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 
-    # Test program
+    # compile test
     '$(TARGET)-g++' \
         -W -Wall -Werror -ansi -pedantic \
-        '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-poppler.exe' \
-        `'$(TARGET)-pkg-config' poppler poppler-cpp --cflags --libs`
+        '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
+        `'$(TARGET)-pkg-config' poppler-cpp --cflags --libs`
 endef
