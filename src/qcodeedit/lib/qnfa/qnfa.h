0b76bdcd3d00c56eb69ef43fd5887fb7d857a164
MeanSquaredError <35379301+MeanSquaredError@users.noreply.github.com>
2019-10-14 20:49:50 +0300

    ws

--- src/qcodeedit/lib/qnfa/qnfa.h
+++ src/qcodeedit/lib/qnfa/qnfa.h
@@ -3,7 +3,7 @@
 ** Copyright (C) 2006-2009 fullmetalcoder <fullmetalcoder@hotmail.fr>
 **
 ** This file is part of the Edyuk project <http://edyuk.org>
-** 
+**
 ** This file may be used under the terms of the GNU General Public License
 ** version 3 as published by the Free Software Foundation and appearing in the
 ** file GPL.txt included in the packaging of this file.
@@ -42,45 +42,45 @@ class QNFABranch : public light_vector<QNFA*>
 enum NFAType
 {
 	Char			= 0,
-	
+
 	Match			= 1,
-	
+
 	CxtBeg			= 2,
 	CxtEnd			= 4,
 	CxtEsc			= 8,
-	
+
 	ContextBegin	= Match | CxtBeg,
 	ContextEnd		= Match | CxtEnd,
 	EscapeSeq		= Match | CxtEsc,
-	
+
 	Escaped			= 16,
 	Exclusive		= 32,
 	StayOnLine		= 64,
-	
+
 	Reserved		= 128
 };
 
 enum NFAAssertion
 {
 	NoAssertion		= 0,
-	
+
 	One				= 0,		// default standard
 	ZeroOrOne		= 1,		// ?
 	ZeroOrMore		= 2,		// *
 	OneOrMore		= 4,		// +
-	
+
 	WordStart		= 8,
 	WordEnd			= 16,
-	
+
 	Word			= 32,
 	NonWord			= 64,
-	
+
 	Digit			= 128,
 	NonDigit		= 256,
-	
+
 	Space			= 512,
 	NonSpace		= 1024,
-	
+
 	CaseSensitive	= 2048,
 
 	LineStart = 4096
@@ -94,13 +94,13 @@ struct QCharTreeNode
 {
 	inline QCharTreeNode(quint16 v = 0) { value.unicode = v; }
 	inline QCharTreeNode(const QCharTreeNode& o) { value = o.value; next = o.next; }
-	
+
 	union
 	{
 		int action;
 		quint16 unicode;
 	} value;
-	
+
 	QCharTreeLevel next;
 };
 
@@ -112,28 +112,28 @@ struct QNFA
 {
 	QNFA();
 	~QNFA();
-	
+
 	QNFASet c;
 	QCharTree tree;
-	
+
 	union
 	{
 		QNFA *next;
 		QNFABranch *branch;
 	} out;
-	
+
 	quint8 type;
 	quint16 assertion;
-	
+
 	int actionid;
-	
+
 	static quint32 _count;
 };
 
 struct QNFAMatchContext
 {
 	inline QNFAMatchContext(QNFA *root = 0) : context(root) {}
-	
+
 	inline QNFAMatchContext& operator = (QNFAMatchContext *c)
 	{
 		if ( c )
@@ -144,27 +144,27 @@ struct QNFAMatchContext
 		} else {
 			reset();
 		}
-		
+
 		return *this;
 	}
-	
+
 	inline QNFAMatchContext& operator = (const QNFAMatchContext& c)
 	{
 		context = c.context;
 		parents = c.parents;
 		meaningless = c.meaningless;
-		
+
 		return *this;
 	}
-	
+
 	inline void reset()
 	{
 		context = 0;
-		
+
 		while ( parents.count() )
 			context = parents.pop();
 	}
-	
+
 	QNFA *context;
 	QNFASet meaningless;
 	QStack<QNFA*> parents;
@@ -174,7 +174,7 @@ class QNFAMatchHandler
 {
 	public:
 		virtual ~QNFAMatchHandler() {}
-		
+
 		virtual void matched(int pos, int len, int action) = 0;
 };
 
@@ -185,25 +185,25 @@ class QNFAMatchNotifier
 		{
 			inline Command(int p, int len, int act)
 			 : pos(p), length(len), action(act) {}
-			
+
 			int pos;
 			int length;
 			int action;
 		};
-		
+
 		typedef QList<Command> CommandList;
-		
+
 	public:
 		inline QNFAMatchNotifier(QNFAMatchHandler *h)
 		 : handler(h) {}
-		
+
 		inline QNFAMatchNotifier& operator = (const QNFAMatchNotifier& n)
 		{
 			handler = n.handler;
-			
+
 			return *this;
 		}
-		
+
 		inline void operator () (int pos, int len, int action)
 		{
 			if ( handler && (m_buffers.isEmpty() || m_pending.count()) )
@@ -211,39 +211,39 @@ class QNFAMatchNotifier
 			else
 				m_buffers.top() << Command(pos, len, action);
 		}
-		
+
 		inline int bufferLevel() const
 		{
 			return m_buffers.count();
 		}
-		
+
 		inline void startBuffering()
 		{
 			m_buffers.push(CommandList());
 		}
-		
+
 		inline void stopBuffering()
 		{
 			m_pending = m_buffers.pop();
 		}
-		
+
 		inline void flush()
 		{
 			foreach ( Command c, m_pending )
 				handler->matched(c.pos, c.length, c.action);
-			
+
 			m_pending.clear();
 		}
-		
+
 		inline void clear()
 		{
 			m_pending.clear();
 			m_buffers.clear();
 		}
-		
+
 	private:
 		QNFAMatchHandler *handler;
-		
+
 		CommandList m_pending;
 		QStack<CommandList> m_buffers;
 };
