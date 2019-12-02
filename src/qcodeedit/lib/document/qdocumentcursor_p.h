a66ba2fc23b382870a239fb3d66d86bfc8938aae
MeanSquaredError <35379301+MeanSquaredError@users.noreply.github.com>
2019-12-03 01:06:26 +0200

    Fix source code whitespace formatting and end of line

--- src/qcodeedit/lib/document/qdocumentcursor_p.h
+++ src/qcodeedit/lib/document/qdocumentcursor_p.h
@@ -3,7 +3,7 @@
 ** Copyright (C) 2006-2009 fullmetalcoder <fullmetalcoder@hotmail.fr>
 **
 ** This file is part of the Edyuk project <http://edyuk.org>
-** 
+**
 ** This file may be used under the terms of the GNU General Public License
 ** version 3 as published by the Free Software Foundation and appearing in the
 ** file GPL.txt included in the packaging of this file.
@@ -47,7 +47,7 @@ class QCE_EXPORT QDocumentCursorHandle
 	friend class QDocumentCursor;
 	friend class QDocumentPrivate;
 	friend class QDocumentCommand;
-	
+
 	public:
 		enum Flags
 		{
@@ -61,38 +61,38 @@ class QCE_EXPORT QDocumentCursorHandle
 		};
 
 		QDocument* document() const;
-		
+
 		bool atEnd() const;
 		bool atStart() const;
-		
+
 		bool atBlockEnd() const;
 		bool atBlockStart() const;
-		
+
 		bool atLineEnd() const;
 		bool atLineStart() const;
-		
+
 		bool hasSelection() const;
 		bool isForwardSelection() const;
-		
+
 		bool isSilent() const;
 		void setSilent(bool y);
-		
+
 		bool isAutoUpdated() const;
 		void setAutoUpdated(bool y);
 
 		bool isAutoErasable() const;
 		void setAutoErasable(bool y);
 
-		
+
 		QDocumentLine line() const;
 		QDocumentLine anchorLine() const;
-		
+
 		int lineNumber() const;
 		int columnNumber() const;
-		
+
 		int anchorLineNumber() const;
 		int anchorColumnNumber() const;
-		
+
 		int startLineNumber() const;
 		int startColumnNumber() const;
 
@@ -100,7 +100,7 @@ class QCE_EXPORT QDocumentCursorHandle
 		int endColumnNumber() const;
 
 		int visualColumnNumber() const;
-		
+
 		void setLineNumber(int l, int m = QDocumentCursor::MoveAnchor);
 		void setColumnNumber(int c, int m = QDocumentCursor::MoveAnchor);
 		void setAnchorLineNumber(int l);
@@ -108,45 +108,45 @@ class QCE_EXPORT QDocumentCursorHandle
 
 		QPoint documentPosition() const;
 		QPoint anchorDocumentPosition() const;
-		
+
 		QPolygon documentRegion() const;
-		
+
 		void shift(int offset);
 		void setPosition(int pos, int m);
 		bool movePosition(int offset, int op, const QDocumentCursor::MoveMode& m);
-		
+
 		void insertText(const QString& s, bool keepAnchor = false);
-		
+
 		QChar nextChar() const;
 		QChar previousChar() const;
-		
+
 		void eraseLine();
 		void deleteChar();
 		void deletePreviousChar();
-		
+
 		QDocumentCursor selectionStart() const;
 		QDocumentCursor selectionEnd() const;
 		QDocumentCursor anchorCursor() const;
-		
+
 		bool eq(const QDocumentCursorHandle *h);
 		bool lt(const QDocumentCursorHandle *h);
 		bool gt(const QDocumentCursorHandle *h);
-		
+
 		QString selectedText() const;
-		
+
 		void clearSelection();
 		void flipSelection();
 		void removeSelectedText(bool keepAnchor = false);
 		void replaceSelectedText(const QString& text);
-		
+
 		void select(QDocumentCursor::SelectionType t);
 		void expandSelect(QDocumentCursor::SelectionType t);
 		void setSelectionBoundary(const QDocumentCursor& c);
 		void select(int line, int column, int lineTo = -1, int columnTo = -1);
-		
+
 		bool isWithinSelection(const QDocumentCursor& c) const;
 		QDocumentCursor intersect(const QDocumentCursor& c) const;
-		
+
 		void beginBoundary(int& begline, int& begcol) const;
 		void endBoundary(int& endline, int& endcol) const;
 		void substractBoundaries(int lbeg, int cbeg, int lend, int cend);
@@ -155,29 +155,29 @@ class QCE_EXPORT QDocumentCursorHandle
 		void intersectBoundaries(QDocumentCursorHandle *h, int& lbeg, int& cbeg, int& lend, int& cend) const;
 		bool equalBoundaries(const QDocumentCursorHandle* c);
 		bool equal(const QDocumentCursorHandle* c);
-			
-		
+
+
 		void getMatchingPair(QDocumentCursor& from, QDocumentCursor& to, bool maximal) const;
 
 		void beginEditBlock();
 		void endEditBlock();
-		
+
 		void moveTo(int line, int column, const QDocumentCursor::MoveMode& m =0);
 		void moveTo(const QDocumentCursor &c, const QDocumentCursor::MoveMode& m );
-		
+
 		void copy(const QDocumentCursorHandle *c);
-		
+
 		void refreshColumnMemory();
 		bool hasColumnMemory() const;
 		void setColumnMemory(bool y);
-		
+
 		bool isRTL() const;
 
 		virtual void execute(QDocumentCommand *c);
-		
+
 		inline void ref() { m_ref.ref(); }
-        inline void deref() { if ( !m_ref.deref() ) delete this; }
-		
+		inline void deref() { if ( !m_ref.deref() ) delete this; }
+
 		inline bool hasFlag(int f) const { return m_flags & f; }
 		inline void setFlag(int f) { m_flags |= f; }
 		inline void clearFlag(int f) { m_flags &= ~f; }
@@ -185,9 +185,9 @@ class QCE_EXPORT QDocumentCursorHandle
 		QDocumentCursorHandle(QDocument *d, int line = 0);
 		QDocumentCursorHandle(QDocument *d, int line, int column, int lineTo = -1, int columnTo = -1);
 		virtual ~QDocumentCursorHandle();
-		
+
 		QDocumentCursorHandle* clone(bool cloneAutoUpdatedFlag) const;
-		
+
 	private:
 		int m_flags;
 		QDocument *m_doc;
