bf02629a670141eb9dcb037f7fbcefd832cb98b9
Jan Sundermeyer <sunderme@gmx.de>
2019-12-27 10:21:11 +0100

    remove qt4 code path

--- src/buildmanager.cpp
+++ src/buildmanager.cpp
@@ -356,11 +356,8 @@ void BuildManager::initDefaultCommandNames()
 void BuildManager::checkOSXElCapitanDeprecatedPaths(QSettings &settings, const QStringList &commands)
 {
 #ifdef Q_OS_MAC
-#if QT_VERSION >= 0x050500
 	if (QSysInfo::MacintoshVersion == QSysInfo::MV_10_11) {
-#else  // older Qt versions don't know MV_10_11
-	if (QSysInfo::MacintoshVersion > QSysInfo::MV_10_10) {
-#endif
+
 		ConfigManagerInterface *config = ConfigManagerInterface::getInstance();
 		if (!config->getOption("Tools/CheckOSXElCapitanDeprecatedPaths", true).toBool()) {
 			return;
