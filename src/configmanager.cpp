b44d94667522d6d8fd4e9c353c706629e03dcc84
Jan Sundermeyer <sunderme@gmx.de>
2020-05-21 20:29:43 +0200

    detect light-/darkmode setting from used styles

--- src/configmanager.cpp
+++ src/configmanager.cpp
@@ -2659,19 +2659,21 @@ void ConfigManager::setInterfaceStyle()
 
 	if (!QStyleFactory::keys().contains(newStyle)) newStyle = defaultStyleName;
 
+	if (modernStyle) {
+		ManhattanStyle *style = new ManhattanStyle(newStyle);
+		if (style->isValid()) QApplication::setStyle(style);
+	} else
+        QApplication::setStyle(newStyle);
+
     // dark mode is derived from system text color (very light => dark mode)
     // however if system colors are ignored, only style manhattan - dark results in dark mode
+    // do the check after setting style, as the style can also activate a dark mode
     if(useTexmakerPalette){
         darkMode=modernStyle>1;
     }else{
         darkMode=systemUsesDarkMode();
     }
 
-	if (modernStyle) {
-		ManhattanStyle *style = new ManhattanStyle(newStyle);
-		if (style->isValid()) QApplication::setStyle(style);
-	} else
-        QApplication::setStyle(newStyle);
     QPalette pal = systemPalette;
     if (useTexmakerPalette) { //modify palette like texmaker does it
         if(darkMode){
