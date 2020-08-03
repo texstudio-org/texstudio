946ffe89a0573bfb1264549f398af2c8265fa15c
Jan Sundermeyer <sunderme@gmx.de>
2020-08-03 20:32:20 +0200

    update osx build on travis

--- travis-ci/get-dependencies.sh
+++ travis-ci/get-dependencies.sh
@@ -49,16 +49,17 @@ if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
 		fi
 	fi
 elif [ "${TRAVIS_OS_NAME}" = "osx" ]; then
-#	print_info "Updating homebrew"
-#	brew update > brew_update.log || { print_error "Updating homebrew failed"; cat brew_update.log; exit 1; }
+	print_info "Updating homebrew"
+	brew update > brew_update.log || { print_error "Updating homebrew failed"; cat brew_update.log; exit 1; }
 # apparently brew comes now with preinstalled qt & poppler
 #	print_info "Brewing packages: qt5 poppler"
 #	brew install qt5
 #	brew uninstall poppler
 #	brew unlink python@2
     brew uninstall --ignore-dependencies poppler
-	brew install -v "${TRAVIS_BUILD_DIR}/travis-ci/mac/poppler.rb"
-	print_info "poppler compiled"
+    brew pin poppler
+	brew install -f "${TRAVIS_BUILD_DIR}/travis-ci/mac/poppler.rb"
+#	brew link poppler 0.84.0-texworks
 else
 	print_error "Unsupported host/target combination '${TRAVIS_OS_NAME}'"
 	exit 1
