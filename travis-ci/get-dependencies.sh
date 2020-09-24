3cdb0b8dca473c7fa94fb951d14f81125b932ca3
Jan Sundermeyer <sunderme@gmx.de>
2020-09-24 21:41:57 +0200

    try fix

--- travis-ci/get-dependencies.sh
+++ travis-ci/get-dependencies.sh
@@ -11,7 +11,7 @@ print_headline "Getting dependencies for building for ${QT} on ${TRAVIS_OS_NAME}
 
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
 	if [ $QT = "qt5win" ]; then
-		MXEDIR="/opt/mxe"
+		MXEDIR="/usr/lib/mxe"
 		MXETARGET="i686-w64-mingw32.static"
 
 		echo "MXEDIR=\"${MXEDIR}\"" >> travis-ci/defs.sh
