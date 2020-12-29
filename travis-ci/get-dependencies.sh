e47404de5d3f7087282f177fc3c9f55ed3e2baf9
Jan Sundermeyer <sdm@Jans-Mac-mini.fritz.box>
2020-12-29 13:51:28 +0100

    try to fix libpoppler internal links to fix macdeployqt

--- travis-ci/get-dependencies.sh
+++ travis-ci/get-dependencies.sh
@@ -54,9 +54,17 @@ elif [ "${TRAVIS_OS_NAME}" = "osx" ]; then
 #	brew install qt5
 #	brew uninstall poppler
 #	brew unlink python@2
-    brew uninstall --ignore-dependencies poppler
-	brew install -f "${TRAVIS_BUILD_DIR}/travis-ci/mac/poppler.rb"
-	brew switch poppler 20.09.0-texworks
+#    brew uninstall --ignore-dependencies poppler
+#	brew install -f "${TRAVIS_BUILD_DIR}/travis-ci/mac/poppler.rb"
+#	brew switch poppler 20.09.0-texworks
+## fix internal links in libpoppler-qt5 as it messes with macdeployqt
+	cd /usr/local/opt/poppler/lib
+	chmod u+w  libpoppler-qt5.dylib
+	a=`otool -L libpoppler-qt5.dylib|grep Cellar|sed -e 's/ .*//' -e 's/	//'`
+	b=`echo $a|sed -e 's#/.*/##'`
+	c=`pwd`/$b
+	install_name_tool -change $a $c libpoppler-qt5.1.dylib
+	cd
 else
 	print_error "Unsupported host/target combination '${TRAVIS_OS_NAME}'"
 	exit 1
