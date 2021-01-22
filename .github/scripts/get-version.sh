d30fbfbfcb435270f74935672d557f716f15ddf6
Jan Sundermeyer <sunderme@gmx.de>
2021-01-22 23:23:10 +0100

    test

--- .github/scripts/get-version.sh
+++ .github/scripts/get-version.sh
@@ -1,26 +1,26 @@
 # GNU extensions for sed are not supported; on Linux, --posix mimics this behaviour
-TXS_VERSION=$(sed -ne 's/^#define TXSVERSION "\(.*\)".*$/\1/p' src/utilsVersion.h)
+export TXS_VERSION=$(sed -ne 's/^#define TXSVERSION "\(.*\)".*$/\1/p' src/utilsVersion.h)
 echo "TXS_VERSION = ${TXS_VERSION}"
 
-GIT_HASH=$(git --git-dir=".git" show --no-patch --pretty="%h")
+export GIT_HASH=$(git --git-dir=".git" show --no-patch --pretty="%h")
 echo "GIT_HASH = ${GIT_HASH}"
 
-GIT_DATE=$(git --git-dir=".git" show --no-patch --pretty="%ci")
+export GIT_DATE=$(git --git-dir=".git" show --no-patch --pretty="%ci")
 echo "GIT_DATE = ${GIT_DATE}"
 
-DATE_HASH=$(date -u +"%Y%m%d%H%M")
+export DATE_HASH=$(date -u +"%Y%m%d%H%M")
 echo "DATE_HASH = ${DATE_HASH}"
-OS_NAME=$(uname)
+export OS_NAME=$(uname)
 echo ${OS_NAME}
 if [ "${OS_NAME}" = "Linux" ]; then
-	RELEASE_DATE=$(date -u +"%Y-%m-%dT%H:%M:%S%z" --date="${GIT_DATE}")
+	export RELEASE_DATE=$(date -u +"%Y-%m-%dT%H:%M:%S%z" --date="${GIT_DATE}")
 elif [ "${OS_NAME}" = "OSX" ]; then
-	RELEASE_DATE=$(date -ujf "%Y-%m-%d %H:%M:%S %z" "${GIT_DATE}" "+%Y-%m-%dT%H:%M:%S%z")
+	export RELEASE_DATE=$(date -ujf "%Y-%m-%d %H:%M:%S %z" "${GIT_DATE}" "+%Y-%m-%dT%H:%M:%S%z")
 else
 	echo "Unsupported operating system '${OS_NAME}'"
 	exit 1
 fi
 echo "RELEASE_DATE = ${RELEASE_DATE}"
 
-VERSION_NAME="${TXS_VERSION}-${DATE_HASH}-git_${GIT_HASH}"
+export VERSION_NAME="${TXS_VERSION}-${DATE_HASH}-git_${GIT_HASH}"
 echo "VERSION_NAME = ${VERSION_NAME}"
