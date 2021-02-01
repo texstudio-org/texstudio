c651aa8ddea10929555a69643a0700c3055c44ec
Murdock Grewar <mggrewar@hotmail.com>
2021-02-01 23:46:21 +1100

    Hotfix: limit PDF render threads to 1. Appears that it might not affect render time. Prevents suspected threading issue causing pdf-viewing crashes on machines with high core count.

--- src/pdfviewer/PDFDocument.cpp
+++ src/pdfviewer/PDFDocument.cpp
@@ -3086,7 +3086,7 @@ retryNow:
         renderManager = nullptr;
 	}
 
-	renderManager = new PDFRenderManager(this);
+	renderManager = new PDFRenderManager(this, 1);
 	renderManager->setCacheSize(globalConfig->cacheSizeMB);
 	renderManager->setLoadStrategy(int(globalConfig->loadStrategy));
 	PDFRenderManager::Error error = PDFRenderManager::NoError;
