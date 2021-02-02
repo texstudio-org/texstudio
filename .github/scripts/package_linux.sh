57c7d3dfd2b860e45606ac267b4a1e6f1ceb7ab0
Jan Sundermeyer <sunderme@gmx.de>
2021-02-02 19:45:48 +0100

    try tweak for appimage

--- .github/scripts/package_linux.sh
+++ .github/scripts/package_linux.sh
@@ -12,7 +12,7 @@ wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/contin
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=linux-${VERSION_NAME}
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -extra-plugins=iconengines/libqsvgicon.so
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -extra-plugins=iconengines/libqsvgicon.so,iconengines,platformthemes/libqgtk3.so
 ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
 cp TeXstudio-${VERSION}-x86_64.AppImage texstudio-${VERSION}-x86_64.AppImage
 cp TeXstudio-${VERSION}-x86_64.AppImage texstudio-${TXS_VERSION}-x86_64.AppImage
