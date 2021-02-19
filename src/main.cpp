7d69965e08b7c662a0220c456e7a6dd44aafd1f0
Jan Sundermeyer <sunderme@gmx.de>
2021-02-19 10:20:50 +0100

    fix #1491 qt scaling looks ugly at 150% futhermore it messes with adwaita style

--- src/main.cpp
+++ src/main.cpp
@@ -188,11 +188,11 @@ int main(int argc, char **argv)
 {
 	QString appId = generateAppId();
 #if QT_VERSION >= QT_VERSION_CHECK(5,6,0)
-#ifdef Q_OS_WIN
+/*#ifdef Q_OS_WIN
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#else
+#else*/
     QApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
-#endif
+//#endif
 #endif
 	// This is a dummy constructor so that the programs loads fast.
 	TexstudioApp a(appId, argc, argv);
