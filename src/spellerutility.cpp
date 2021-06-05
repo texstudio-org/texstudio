d2c2afd3281efd86f51b2e461c45f35f97cb0069
Jan Sundermeyer <sunderme@gmx.de>
2021-06-05 17:39:36 +0200

    remove obsolete code

--- src/spellerutility.cpp
+++ src/spellerutility.cpp
@@ -174,11 +174,9 @@ bool SpellerUtility::check(QString word)
     if(!pChecker)
         return true;
     QByteArray encodedString = spellCodec->fromUnicode(word);
-#if QT_VERSION >= 0x050400
+
     bool result = pChecker->spell(encodedString.toStdString());
-#else
-    bool result = pChecker->spell(QString(encodedString).toStdString());
-#endif
+
 	return result;
 }
 
@@ -193,22 +191,13 @@ QStringList SpellerUtility::suggest(QString word)
     QMutexLocker locker(&mSpellerMutex);
     if(!pChecker)
         return suggestion;
-#if QT_VERSION >= 0x050400
+
     wlst = pChecker->suggest(encodedString.toStdString());
-#else
-    wlst = pChecker->suggest(QString(encodedString).toStdString());
-#endif
 
     unsigned long ns=wlst.size();
 	if (ns > 0) {
         for (uint i = 0; i < ns; i++) {
-#if QT_VERSION >= 0x050400
             suggestion << spellCodec->toUnicode(QByteArray::fromStdString(wlst[i]));
-#else
-            QString wrd=QString::fromStdString(wlst[i]);
-            suggestion << wrd;//spellCodec->toUnicode(wrd.toUtf8());
-#endif
-
 		}
 	}
 	return suggestion;
