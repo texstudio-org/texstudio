b76d2d9503f836460d7c39023c1acd2d7ba3c4b2
Jan Sundermeyer <sunderme@gmx.de>
2021-07-31 12:26:25 +0200

    fix for qt5.9

--- src/utilsUI.cpp
+++ src/utilsUI.cpp
@@ -439,9 +439,9 @@ int getFmWidth(const QFontMetrics &fm, QChar ch)
 qreal getFmWidth(const QFontMetricsF &fm, const QString &text, int len)
 {
 #if QT_VERSION >= QT_VERSION_CHECK(5, 11, 0)
-	return fm.horizontalAdvance(text, len);
+    return fm.horizontalAdvance(text, len);
 #else
-	return fm.width(text, len);
+    return fm.width(text.left(len));
 #endif
 }
 
