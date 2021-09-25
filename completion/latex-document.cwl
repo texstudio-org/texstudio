texstudio-org/texstudio

commit 711ca02d77d157cc4704268cee26be8641be771e

Cwl changes (#1062)
* cwl - add keyvals list to pdfpages.cwl

* cwl - add opt-arg variants of \parbox

* cwl - minor fix to hlist.cwl

completion/latex-document.cwl
@@ -333,6 +333,8 @@
 \paragraph[short title]{title}#L5
 \paragraphmark
 \paragraph{title}#L5
+\parbox[position][height][inner-pos]{width}{contents}
+\parbox[position][height]{width}{contents}
 \parbox[position]{width}{text}
 \parbox{width}{text}
 \part*{title}#L0

completion/hlist.cwl
@@ -22,7 +22,7 @@ col sep=##L
 item offset=##L
 label sep=##L
 label width=##L
-label align=left,right,center
+label align=#left,right,center
 label=%<code%>
 pre label=%<code%>
 post label=%<code%>
@@ -31,7 +31,7 @@ resume#true,false
 autoindent#true,false
 pre item=%<code%>
 post item=%<code%>
-show label=#true,false
+show label#true,false
 list parindent=##L
 show frame#true,false
 #endkeyvals

completion/pdfpages.cwl
@@ -1,15 +1,66 @@
 # pdfpages package
 # mario 09/06/2007, Tim Hoffmann 25/02/2017
+# muzimuzhi 29 Apr 2020, pdfpages v0.5q
 # URL: http://tug.ctan.org/tex-archive/macros/latex/contrib/pdfpages/
+
+#include:eso-pic
+#include:atbegshi
+#include:pdflscape
 #include:graphicx
+#include:ifthen
 #include:calc
 #include:count1to
-#include:eso-pic
-#include:ifthen
-#include:pdflscape
-\includepdf[options]{filename}
+
+\includepdf[options%keyvals]{filename}
 \includepdf{filename}
-\includepdfmerge[options]{file-list}
+\includepdfmerge[options%keyvals]{file-list}
 \includepdfmerge{file-list}
-\includepdfset{global options}
+\includepdfset{global options%keyvals}
 \threadinfodict#*
+
+#keyvals:\includepdf,includepdfmerge,\includepdfset
+pages=
+nup=%<xnup%>n%<ynup%>
+landscape#true,false
+delta=%<delx%> %<dely%>
+offset=%<offx%> %<offy%>
+frame#true,false
+column#true,false
+columnstrict#true,false
+openright#true,false
+openrighteach#true,false
+pagecommand=%<code%>
+turn#true,false
+noautoscale#true,false
+fitpaper#true,false
+reflect#true,false
+signature=%<size%>
+signature*=%<size%>
+booklet#true,false
+picturecommand=%<code%>
+picturecommand*=%<code%>
+pagetemplate=%<page number%>
+templatesize={%<width%>}{%<height%>}
+rotateoversize#true,false
+doublepages#true,false
+doublepagestwist#true,false
+doublepagestwistodd#true,false
+doublepagestwist*#true,false
+doublepagestwistodd*#true,false
+lastpage=%<page number%>
+link#true,false
+linkname=%<default linkname%>
+thread#true,false
+threadname=
+linktodoc#true,false
+linkfit=
+linktodocfit=
+newwindow#true,false
+linkfilename=
+addtotoc=
+addtolist=
+survey#true,false
+survey-nolink#true,false
+xr-prefix=
+## and all the \includegraphics options
+#endkeyvals

commit 299513eb9226b9416dfcbb9bd090a259cb79f351

cwl: enable spell check in \parbox
Fixed an issue introduced in 711ca02

completion/latex-document.cwl
@@ -331,8 +331,8 @@
 \paragraph[short title]{title}#L5
 \paragraphmark
 \paragraph{title}#L5
-\parbox[position][height][inner-pos]{width}{contents}
-\parbox[position][height]{width}{contents}
+\parbox[position][height][inner-pos]{width}{text}
+\parbox[position][height]{width}{text}
 \parbox[position]{width}{text}
 \parbox{width}{text}
 \part*{title}#L0