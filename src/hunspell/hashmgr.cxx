9040435d9d3d0db45c33ee31c7e5c48cf6c3fbd0
Jan Sundermeyer <sunderme@gmx.de>
2021-10-27 20:35:15 +0200

    revert

--- src/hunspell/hashmgr.cxx
+++ src/hunspell/hashmgr.cxx
@@ -708,7 +708,7 @@ int HashMgr::hash(const char* word) const {
   for (int i = 0; i < 4 && *word != 0; i++)
     hv = (hv << 8) | (*word++);
   while (*word != 0) {
-    hv = rotate(hv,RotateLength);
+    ROTATE(hv, ROTATE_LEN);
     hv ^= (*word++);
   }
   return (unsigned long)hv % tablesize;
