e85190d8d0837bedad6636586b986dc088d93e29
octaeder <102688820+octaeder@users.noreply.github.com>
2022-05-15 14:40:13 +0200

    Luapreview (#2330)
    Allow preview with lualatex.
    Note: it seems that package tikz is source of an unresolved problem with format files (s. texstudio-org#2291). In this case disable "Precompile Preamble" in Preview options).
    Main changes:
    * configdialog.ui: "Preview with lualatex" in drop down list
    * buildmanager.cpp: Amend lualatex (CMD_LUALATEX) with -fmt option to use format file. Add pair of braces since compiler complained about ambiguity.

--- src/texstudio.cpp
+++ src/texstudio.cpp
@@ -8509,7 +8509,7 @@ void Texstudio::showPreview(const QString &text)
 	QStringList header;
 	for (int l = 0; l < m_endingLine; l++)
 		header << edView->editor->document()->line(l).text();
-	if (buildManager.dvi2pngMode == BuildManager::DPM_EMBEDDED_PDF) {
+	if (buildManager.dvi2pngMode == BuildManager::DPM_EMBEDDED_PDF || buildManager.dvi2pngMode == BuildManager::DPM_LUA_EMBEDDED_PDF) {
 		header << "\\usepackage[active,tightpage]{preview}"
 		       << "\\usepackage{varwidth}"
 		       << "\\AtBeginDocument{\\begin{preview}\\begin{varwidth}{\\linewidth}}"
@@ -8609,7 +8609,8 @@ QStringList Texstudio::makePreviewHeader(const LatexDocument *rootDoc)
 			header << newLine;
 		}
 	}
-	if ((buildManager.dvi2pngMode == BuildManager::DPM_EMBEDDED_PDF) && configManager.previewMode != ConfigManager::PM_EMBEDDED) {
+	if ((buildManager.dvi2pngMode == BuildManager::DPM_EMBEDDED_PDF || buildManager.dvi2pngMode == BuildManager::DPM_LUA_EMBEDDED_PDF)
+			&& configManager.previewMode != ConfigManager::PM_EMBEDDED) {
 		header << "\\usepackage[active,tightpage]{preview}"
 		       << "\\usepackage{varwidth}"
 		       << "\\AtBeginDocument{\\begin{preview}\\begin{varwidth}{\\linewidth}}"
