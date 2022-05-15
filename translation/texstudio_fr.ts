02d1ac63aed69a9628854807f05a9b4bbbdf8b4a
Jan Sundermeyer <sunderme@gmx.de>
2022-05-15 21:30:54 +0200

    update translation

--- translation/texstudio_fr.ts
+++ translation/texstudio_fr.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="fr">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="fr">
 <context>
     <name>AboutDialog</name>
     <message>
@@ -886,7 +884,7 @@ Veuillez modifier votre configuration et définir les listes de commandes unique
         <location filename="../src/configdialog.ui" line="513"/>
         <source>Defines the kinds of update notifications you will receive:
 - Stable Releases: Choose this if stability is most important to you.
-- Release Candidates: Are close to a future release in terms of features and stability. Choose this to get previews of future releases and help us by testing the version before it&apos;s been published as an official release. (Stable releases are notifed as well)
+- Release Candidates: Are close to a future release in terms of features and stability. Choose this to get previews of future releases and help us by testing the version before it's been published as an official release. (Stable releases are notifed as well)
 - Development Versions: Contain the latest features, but might be unstable. (Stable releases and release candidates are notified as well).</source>
         <translation>Définit les types de notifications de mises à jour que vous recevrez :
 - versions stables : à choisir si la stabilité est le plus important pour vous ;
@@ -1095,7 +1093,7 @@ Les variables sont de la forme : $MYVAR. Elles sont sensibles à la casse.</tran
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="1087"/>
-        <source>For editing, double-click on the entries in &apos;Current Shortcut&apos; or &apos;Additional Shortcut&apos;.
+        <source>For editing, double-click on the entries in 'Current Shortcut' or 'Additional Shortcut'.
 Then you can select a new shortcut by one of the following ways:
 (1) select from the dropdown list
 (2) hit the shortcut combination
@@ -2398,7 +2396,7 @@ leur usage en les activant ici.</translation>
     <message>
         <location filename="../src/configdialog.ui" line="3779"/>
         <source>Segment Preview</source>
-        <translation>Aperçu de la portion</translation>
+        <translation>Prévisualisation de la portion</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3785"/>
@@ -2408,27 +2406,27 @@ leur usage en les activant ici.</translation>
     <message>
         <location filename="../src/configdialog.ui" line="3796"/>
         <source>Preview with dvipng</source>
-        <translation>Aperçu avec dvipng</translation>
+        <translation>Prévisualisation avec dvipng</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3801"/>
         <source>Preview with dvipng --follow (parallel)</source>
-        <translation>Aperçu avec « dvipng --follow » (en parallèle)</translation>
+        <translation>Prévisualisation avec « dvipng --follow » (en parallèle)</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3806"/>
         <source>Preview with dvips/ghostscript</source>
-        <translation>Aperçu avec dvips/ghostscript</translation>
+        <translation>Prévisualisation avec dvips/ghostscript</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3811"/>
         <source>Preview with pdflatex</source>
-        <translation>Aperçu avec pdflatex</translation>
+        <translation>Prévisualisation avec pdflatex</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3816"/>
         <source>Preview with lualatex</source>
-        <translation type="unfinished"></translation>
+        <translation>Prévisualisation avec lualatex</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3824"/>
@@ -2438,17 +2436,17 @@ leur usage en les activant ici.</translation>
     <message>
         <location filename="../src/configdialog.ui" line="3838"/>
         <source>Show preview as tooltip if panel is hidden</source>
-        <translation>Afficher l&apos;aperçu en info-bulle si le panneau est caché</translation>
+        <translation>Afficher la prévisualisation en info-bulle si le panneau est caché</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3843"/>
         <source>Always show preview in preview panel</source>
-        <translation>Toujours afficher l&apos;aperçu dans le panneau d&apos;aperçu</translation>
+        <translation>Toujours afficher la prévisualisation dans le panneau d&apos;aperçu</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3848"/>
         <source>Always show preview as tool tip</source>
-        <translation>Toujours afficher l&apos;aperçu en info-bulle</translation>
+        <translation>Toujours afficher la prévisualisation en info-bulle</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3853"/>
@@ -2469,7 +2467,7 @@ leur usage en les activant ici.</translation>
     <message>
         <location filename="../src/configdialog.ui" line="3871"/>
         <source>Update the preview on text change</source>
-        <translation>Mettre à jour l&apos;aperçu en cas de modification du texte</translation>
+        <translation>Mettre à jour la prévisualisation en cas de modification du texte</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3874"/>
@@ -2499,7 +2497,7 @@ leur usage en les activant ici.</translation>
     <message>
         <location filename="../src/configdialog.ui" line="3944"/>
         <source>Show preview as tooltip on formulas in editor</source>
-        <translation>Afficher l&apos;aperçu en info-bulle pour les formules dans l&apos;éditeur</translation>
+        <translation>Afficher la prévisualisation en info-bulle pour les formules dans l&apos;éditeur</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="3954"/>
@@ -2647,8 +2645,8 @@ leur usage en les activant ici.</translation>
         <location filename="../src/configdialog.ui" line="4359"/>
         <location filename="../src/configdialog.ui" line="4413"/>
         <source>The Render Backend used by the PDF library poppler.
-&apos;Splash&apos; is the default and suitable for most situations.
-If there are rendering issues, you may try the alternative &apos;Arthur&apos; backend.
+'Splash' is the default and suitable for most situations.
+If there are rendering issues, you may try the alternative 'Arthur' backend.
 Note: Changing this setting will only affect documents that are opened afterwards.</source>
         <translation>Le moteur de rendu utilisé par la bibliothèque PDF poppler.
 « Splash ».est celui par défaut et convient dans la plupart des cas.
@@ -2842,7 +2840,7 @@ to set the key anyway?</source>
         <location filename="../src/configdialog.cpp" line="492"/>
         <location filename="../src/configdialog.cpp" line="532"/>
         <source>Preview</source>
-        <translation>Aperçu</translation>
+        <translation>Prévisualisation</translation>
     </message>
     <message>
         <location filename="../src/configdialog.cpp" line="493"/>
@@ -4414,7 +4412,7 @@ Continuer tout de même ?</translation>
 [p] Page: On a page dedicated to figures
 [h] Here: At this position in the text
 [H] Here: Strictly at this position in the text (requires float package)
-[!]: Override internal parameters LaTeX uses for determining `good&apos; float positions.
+[!]: Override internal parameters LaTeX uses for determining `good' float positions.
 
 Note: The first 4 placement preferences are just suggestions. If the resulting page layout would look bad, LaTeX may ignore this.</source>
         <translation>Préférences de placement pour la figure
@@ -4532,7 +4530,7 @@ l&apos;assistant ne supporte pas la commande</translation>
     </message>
     <message>
         <location filename="../src/latexcompleter.cpp" line="2009"/>
-        <source>&lt;p style=&apos;white-space:pre&apos;&gt;&lt;b&gt;Filename: %1&lt;/b&gt;
+        <source>&lt;p style='white-space:pre'&gt;&lt;b&gt;Filename: %1&lt;/b&gt;
 </source>
         <translation>&lt;p style=&apos;white-space:pre&apos;&gt;&lt;b&gt;Nom de fichier : %1&lt;/b&gt;</translation>
     </message>
@@ -4679,17 +4677,14 @@ l&apos;assistant ne supporte pas la commande</translation>
     <message>
         <location filename="../src/latexeditorview.cpp" line="2540"/>
         <location filename="../src/latexeditorview.cpp" line="2612"/>
-        <source>&lt;p style=&apos;white-space:pre&apos;&gt;&lt;b&gt;Filename: %1&lt;/b&gt;
+        <source>&lt;p style='white-space:pre'&gt;&lt;b&gt;Filename: %1&lt;/b&gt;
 </source>
         <translation>&lt;p style=&apos;white-space:pre&apos;&gt;&lt;b&gt;Nom de fichier : %1&lt;/b&gt;</translation>
     </message>
     <message numerus="yes">
         <location filename="../src/latexeditorview.cpp" line="2553"/>
         <source>%n reference(s) to this label</source>
-        <translation>
-            <numerusform>%n référence à cette étiquette</numerusform>
-            <numerusform>%n références à cette étiquette</numerusform>
-        </translation>
+        <translation><numerusform>%n référence à cette étiquette</numerusform><numerusform>%n références à cette étiquette</numerusform></translation>
     </message>
     <message>
         <location filename="../src/latexeditorview.cpp" line="2558"/>
@@ -5055,7 +5050,7 @@ Voulez-vous l&apos;écraser ?</translation>
         <location filename="../src/toolwidgets.cpp" line="309"/>
         <location filename="../src/toolwidgets.cpp" line="382"/>
         <source>Preview</source>
-        <translation>Aperçu</translation>
+        <translation>Prévisualisation</translation>
     </message>
     <message>
         <location filename="../src/toolwidgets.cpp" line="313"/>
@@ -6389,7 +6384,7 @@ a échoué.</translation>
     <message>
         <location filename="../src/qcodeedit/lib/qeditor.cpp" line="1103"/>
         <source>The original file on disk was destroyed during the save operation.
-You&apos;ll find a copy at
+You'll find a copy at
 %1</source>
         <translation>Le fichier original sur le disque a été détruit lors de l&apos;opération de sauvegarde.
 Une copie est disponible ici :
@@ -8776,7 +8771,7 @@ n&apos;a pas pu être supprimé.</translation>
     <message>
         <location filename="../src/templateselector.cpp" line="34"/>
         <source>No Preview</source>
-        <translation>Pas d&apos;aperçu</translation>
+        <translation>Pas de prévisualisation</translation>
     </message>
     <message>
         <location filename="../src/templateselector.cpp" line="86"/>
@@ -10934,7 +10929,7 @@ ou fermer cet autre fichier avant de pouvoir l&apos;écraser.</translation>
     </message>
     <message>
         <location filename="../src/texstudio.cpp" line="5681"/>
-        <source>Can&apos;t detect the file name.
+        <source>Can't detect the file name.
 You have to save a document before you can compile it.</source>
         <translation>Nom de fichier introuvable
 Vous devez enregistrer le document avant de le compiler.</translation>
