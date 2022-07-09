31283cdb039e3609f8df488b0e8ad8c4a5dd1b74
octaeder <102688820+octaeder@users.noreply.github.com>
2022-07-09 06:55:38 +0200

    scroll hand with hot spot mark (new file)
    * s. #2201, file is focusedhand.png
    * changed hot spot coordinate for magnifier to center of the glass

--- src/pdfviewer/PDFDocument.cpp
+++ src/pdfviewer/PDFDocument.cpp
@@ -538,6 +538,7 @@ void PDFMovie::seekDialog()
 QCursor *PDFWidget::magnifierCursor = nullptr;
 QCursor *PDFWidget::zoomInCursor = nullptr;
 QCursor *PDFWidget::zoomOutCursor = nullptr;
+QCursor *PDFWidget::focusedHandCursor = nullptr;
 
 PDFWidget::PDFWidget(bool embedded)
 	: QLabel()
@@ -614,6 +615,7 @@ PDFWidget::PDFWidget(bool embedded)
 		magnifierCursor = new QCursor(QPixmap(getRealIconFile("magnifier")).scaled(32, 32, Qt::KeepAspectRatio, Qt::SmoothTransformation),10,10);
 		zoomInCursor = new QCursor(QPixmap(getRealIconFile("zoom-in")).scaled(32, 32, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 		zoomOutCursor = new QCursor(QPixmap(getRealIconFile("zoom-out")).scaled(32, 32, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+		focusedHandCursor = new QCursor(QPixmap(getRealIconFile("focusedhand")),8,13);
 	}
 
 	ctxZoomInAction = new QAction(tr("Zoom In"), this);
@@ -1559,7 +1561,7 @@ void PDFWidget::updateCursor()
 				setCursor(Qt::ArrowCursor);
 		}
 		else
-			setCursor(Qt::OpenHandCursor);
+			setCursor(*focusedHandCursor);
 	}
 	break;
 	case kMagnifier: {
