64ccd8b5b2df0846e3747a31d40faf9e95938ca3
Jan Sundermeyer <sunderme@gmx.de>
2022-09-25 18:45:42 +0200

    sidebar toc

--- utilities/manual/convertToHTML.sh
+++ utilities/manual/convertToHTML.sh
@@ -1,2 +1,2 @@
-pandoc usermanual_en.md -s --css usermanual.css -t html -f markdown -N --toc -o usermanual_en_test.html
+pandoc usermanual_en.md -s --css usermanual.css --template template.html -t html -f markdown -N --toc -o usermanual_en_test.html
 
