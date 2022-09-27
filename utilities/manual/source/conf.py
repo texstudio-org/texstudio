bb3b325efe63622317d72b2008a33fcfb0945d32
Jan Sundermeyer <sunderme@gmx.de>
2022-09-27 21:14:05 +0200

    update version number

--- utilities/manual/source/conf.py
+++ utilities/manual/source/conf.py
@@ -9,7 +9,7 @@
 project = 'TeXstudio'
 copyright = '2022, TeXstudio Team'
 author = 'TeXstudio Team'
-release = '4.3.2'
+release = '4.4.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
