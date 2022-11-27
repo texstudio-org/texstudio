b8ab09194d92a91b4df0cd100d7b0f1f5e016f8f
Jan Sundermeyer <sunderme@gmx.de>
2022-11-27 12:26:20 +0100

    increase version number

--- src/utilsVersion.h
+++ src/utilsVersion.h
@@ -4,9 +4,9 @@
 #define TEXSTUDIO "TeXstudio"
 
 
-#define TXSVERSION "4.4.1"
+#define TXSVERSION "4.4.2"
 
-#define TXSVERSION_NUMERIC 0x040401
+#define TXSVERSION_NUMERIC 0x040402
 
 extern const char *TEXSTUDIO_GIT_REVISION;
 
