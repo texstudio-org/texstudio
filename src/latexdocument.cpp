859b0f35e2628fa15ed0b9e75692a317e2ca0865
Jan Sundermeyer <sunderme@gmx.de>
2023-01-18 20:56:41 +0100

    fix #2856

--- src/latexdocument.cpp
+++ src/latexdocument.cpp
@@ -544,7 +544,7 @@ bool LatexDocument::patchStructure(int linenr, int count, bool recheck)
             }
             if(!l_tkFilter.isEmpty()){
                 i=i-ConfigManager::RUNAWAYLIMIT;
-                lastHandle = line(i - 1).handle();
+                lastHandle = line(i).handle();
                 if (lastHandle) {
                     oldRemainder = lastHandle->getCookieLocked(QDocumentLine::LEXER_REMAINDER_COOKIE).value<TokenStack >();
                     oldCommandStack = lastHandle->getCookieLocked(QDocumentLine::LEXER_COMMANDSTACK_COOKIE).value<CommandStack >();
