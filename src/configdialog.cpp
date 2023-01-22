c3a251ef4defb65c7ccd33fb141dbd6b255993a1
gaplin <gaplinn@gmail.com>
2023-01-22 18:01:15 +0100

    using frameGeometry to calculate maximum width and height of configDialog

--- src/configdialog.cpp
+++ src/configdialog.cpp
@@ -594,25 +594,27 @@ ConfigDialog::ConfigDialog(QWidget *parent): QDialog(parent,Qt::Dialog|Qt::Windo
 	connect(ui.tbRevertIcon, SIGNAL(clicked()), this, SLOT(revertClicked()));
 	connect(ui.tbRevertCentralIcon, SIGNAL(clicked()), this, SLOT(revertClicked()));
 	connect(ui.tbRevertSymbol, SIGNAL(clicked()), this, SLOT(revertClicked()));
-    connect(ui.tbRevertPDF, SIGNAL(clicked()), this, SLOT(revertClicked()));
+    connect(ui.tbRevertPDF, SIGNAL(clicked()), this, SLOT(revertClicked()));	
+}
 
-	// limit dialog size
+
+void ConfigDialog::showAndLimitSize() {
+	show();
 	QRect screen = QGuiApplication::primaryScreen()->availableGeometry();
 	if (!screen.isEmpty()) {
-		int nwidth = width(), nheight = height();
-		if (nwidth > screen.width()) nwidth = screen.width();
-		if (nheight > screen.height()) nheight = screen.height();
+		int nwidth = width(), nheight = height(),
+			heightDiff = frameGeometry().height() - geometry().height(),
+			maxHeight = screen.height() - heightDiff, maxWidth = screen.width();
+
+		if (nwidth > maxWidth) nwidth = maxWidth;
+		if (nheight > maxHeight) nheight = maxHeight;
 		if (nwidth == width() && nheight == height()) return;
-		resize(nwidth - 32, nheight - 32);
+
+		resize(nwidth, nheight);
+		move(frameGeometry().right() > screen.right() ? screen.left() : x(),
+		   frameGeometry().bottom() > screen.bottom() ? screen.top() : y());	
 	}
 }
-
-void ConfigDialog::moveIfExceedsScreen() {
-	QRect screen = QGuiApplication::primaryScreen()->availableGeometry();
-	move(frameGeometry().right() > screen.right() ? screen.left() : x(),
-		   frameGeometry().bottom() > screen.bottom() ? screen.top() : y());
-}
-
 
 ConfigDialog::~ConfigDialog()
 {
