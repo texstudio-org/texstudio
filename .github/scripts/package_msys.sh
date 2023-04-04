d41e99f2fbb06a5f1dfa1d66abe5c81a39001a5a
Jan Sundermeyer <sunderme@gmx.de>
2023-04-04 20:28:01 +0200

    workaround for current windeployqt bug

--- .github/scripts/package_msys.sh
+++ .github/scripts/package_msys.sh
@@ -16,7 +16,8 @@ mkdir -p package-zip
 cp texstudio.exe package-zip/
 cd package-zip
 #cp /mingw64/bin/libicudt68.dll /mingw64/bin/icudt68.dll
-windeployqt-qt6 texstudio.exe
+# copy of translations apparently broken currently
+windeployqt-qt6  --no-qml --qmlcore --no-translations texstudio.exe
 echo "copy dlls"
 #ldd texstudio.exe | awk '{print $3}'| grep ming | xargs -I{} cp -u {} .
 ldd texstudio.exe | awk '{print $3}'| grep ucrt64 | xargs -I{} cp -u {} .
