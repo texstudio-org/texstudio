0c2efeea9a24eba3e442001b7a269c7073a8bb62
Jan Sundermeyer <sunderme@gmx.de>
2024-08-29 21:19:38 +0200

    remove legacy code

--- src/loghighlighter.cpp
+++ src/loghighlighter.cpp
@@ -20,20 +20,20 @@ LogHighlighter::LogHighlighter(QTextDocument *parent)
 
 void LogHighlighter::highlightBlock(const QString &text)
 {
-	static QRegExp rxLatexError("\\s*! (.*)");
-	static QRegExp rxBadBox("\\s*(Over|Under)(full \\\\[hv]box .*)");
-	static QRegExp rxWarning("\\s*(((! )?(La|pdf|Lua)TeX)|Package) .*Warning.*:(.*)");
-	static QRegExp rxOnlyStars("\\*+\\s*");
-	static QRegExp rxOnlyExclMarks("!+\\s*");
-	static QRegExp rxExclDots("!\\.+\\s*");
-	static QRegExp rxOnlyDots("\\.+\\s*");
-	if (rxLatexError.exactMatch(text) || text == "! " || rxOnlyExclMarks.exactMatch(text) || rxExclDots.exactMatch(text)) {
+    static QRegularExpression rxLatexError("^\\s*! (.*)$");
+    static QRegularExpression rxBadBox("^\\s*(Over|Under)(full \\\\[hv]box .*)$");
+    static QRegularExpression rxWarning("^\\s*(((! )?(La|pdf|Lua)TeX)|Package) .*Warning.*:(.*)$");
+    static QRegularExpression rxOnlyStars("^\\*+\\s*$");
+    static QRegularExpression rxOnlyExclMarks("^!+\\s*$");
+    static QRegularExpression rxExclDots("^!\\.+\\s*$");
+    static QRegularExpression rxOnlyDots("^\\.+\\s*$");
+    if (text.indexOf(rxLatexError)==0 || text == "! " || text.indexOf(rxOnlyExclMarks)==0 || text.indexOf(rxExclDots)==0) {
 		setFormat(0, text.length(), LatexLogEntry::textColor(LT_ERROR));
-	} else if (rxBadBox.exactMatch(text)) {
+    } else if (text.indexOf(rxBadBox)==0) {
 		setFormat(0, text.length(), LatexLogEntry::textColor(LT_BADBOX));
-	} else if (rxWarning.exactMatch(text) || text.startsWith("* ") || rxOnlyStars.exactMatch(text)) {
+    } else if (text.indexOf(rxWarning)==0 || text.startsWith("* ") || text.indexOf(rxOnlyStars)==0) {
 		setFormat(0, text.length(), LatexLogEntry::textColor(LT_WARNING));
-	} else if (text.startsWith(". ") || rxOnlyDots.exactMatch(text)) {
+    } else if (text.startsWith(". ") || text.indexOf(rxOnlyDots)==0) {
 		setFormat(0, text.length(), LatexLogEntry::textColor(LT_INFO));
 	} else if (text.indexOf(".tex", 0) != -1 && !text.startsWith("Error:")) {
 		setFormat(0, text.length(), ColorFile);
