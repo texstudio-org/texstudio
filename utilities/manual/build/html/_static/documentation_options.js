dd3db66b131f4785c88a9503d3ac5d9aa0e41a71
Jan Sundermeyer <sunderme@gmx.de>
2024-09-20 23:11:11 +0200

    update manual

--- utilities/manual/build/html/_static/documentation_options.js
+++ utilities/manual/build/html/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.8.2',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '4.8.4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- utilities/manual/build/html/_static/tabs.js
+++ utilities/manual/build/html/_static/tabs.js
@@ -2,6 +2,9 @@ var labels_by_text = {};
 
 function ready() {
   var li = document.getElementsByClassName("tab-label");
+  const urlParams = new URLSearchParams(window.location.search);
+  const tabs = urlParams.getAll("tabs");
+
   for (const label of li) {
     label.onclick = onLabelClick;
     const text = label.textContent;
@@ -9,6 +12,12 @@ function ready() {
       labels_by_text[text] = [];
     }
     labels_by_text[text].push(label);
+  }
+
+  for (const tab of tabs) {
+    for (label of labels_by_text[tab]) {
+      label.previousSibling.checked = true;
+    }
   }
 }
 
