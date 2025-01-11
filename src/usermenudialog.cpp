1732e64b2d91e194ab9df9070bc01dbab478c86d
Jan Sundermeyer <sunderme@gmx.de>
2025-01-11 16:29:31 +0100

    add missing trigger expression
    fix validity check of user macro trigger expressions

--- src/usermenudialog.cpp
+++ src/usermenudialog.cpp
@@ -577,7 +577,7 @@ void UserMenuDialog::abbrevChanged()
 void UserMenuDialog::triggerChanged()
 {
     // check if trigger is a valid regex
-    const QStringList fixedTriggers{"?txs-start","?new-file","?new-from-template","?load-file","?load-this-file","?save-file","?close-file","?master-changed","?after-typeset","?after-command-run"};
+    const QStringList fixedTriggers{"?txs-start","?new-file","?new-from-template","?load-file","?load-this-file","?save-file","?close-file","?master-changed","?after-typeset","?after-command-run","?highlighted-as","?not-highlighted-as","?language"};
     const QString text=ui.triggerEdit->text();
     QRegularExpression re(text);
     if(!re.isValid() && !fixedTriggers.contains(text)){
