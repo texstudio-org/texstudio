8a840664e4ebaec4f64d993688d0890032126e0c
Jan Sundermeyer <sunderme@gmx.de>
2025-01-11 15:18:13 +0100

    rely on base style for tab presentation

--- src/txstabwidget.cpp
+++ src/txstabwidget.cpp
@@ -98,7 +98,7 @@ LatexEditorView *TxsTabWidget::editorAt(QPoint p) {
 void TxsTabWidget::setActive(bool active) {
 	if (active == m_active) return;
 	m_active = active;
-	QString baseStyle = "QTabBar::close-button {image: url(:/images-ng/close-tab.svgz)} QTabBar::close-button:hover {image: url(:/images-ng/close-tab-hover.svgz)}";
+    QString baseStyle="";
 	if (active) {
 		setStyleSheet(baseStyle);// + " QTabBar {font-weight: bold;} QTabBar::tab:!selected {font-weight: normal;}");
 	} else {
