29304cfcb40ef02a5619fc6d5bee2612a308d163
Jan Sundermeyer <sunderme@gmx.de>
2025-06-19 10:01:44 +0200

    fix #4095
    avoid recalculating of document height/line numbers with temporarily
    invalid viewer width (0)

--- src/qcodeedit/lib/document/qdocument.cpp
+++ src/qcodeedit/lib/document/qdocument.cpp
@@ -1395,7 +1395,7 @@ void QDocument::clearWidthConstraint()
 */
 void QDocument::setWidthConstraint(int width)
 {
-	if ( m_impl )
+    if ( m_impl && width>0)
         m_impl->setWidth(qMax(0, width));
 }
 
