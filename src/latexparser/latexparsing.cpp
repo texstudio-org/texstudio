acd860b1aaa0ec3d47c771f530e074df3ce77e12
Jan Sundermeyer <sunderme@gmx.de>
2025-10-05 16:50:59 +0200

    add info on keyval command name when more than one keyval args are defined

--- src/latexparser/latexparsing.cpp
+++ src/latexparser/latexparsing.cpp
@@ -759,7 +759,12 @@ bool latexDetermineContexts2(QDocumentLineHandle *dlh, TokenStack &stack, Comman
                 tk.type = Token::keyVal_key;
                 if(!commandStack.isEmpty()){
                     const CommandDescription &cd = commandStack.top();
-                    tk.optionalCommandName=cd.optionalCommandName;
+                    int indexOfKeyValArg=determineKeyValIndex(cd);
+                    if(indexOfKeyValArg>0){
+                        tk.optionalCommandName=cd.optionalCommandName+QString("#o%1").arg(indexOfKeyValArg);
+                    }else{
+                        tk.optionalCommandName=cd.optionalCommandName;
+                    }
                 }
                 keyName = line.mid(tk.start, tk.length);
                 lexed << tk;
@@ -1579,6 +1584,22 @@ TokenList findRestArgTL(QDocumentLineHandle *dlh, Token::TokenType type, int hin
         }
     }
     return tl + findRestArgTL(dlh, type, index+1, count - 1);
+}
+/*!
+ * \brief determine the number of available keyval arguments
+ * In case more than 1 is defined
+ * \param cd
+ * \return number of keyval arguments
+ */
+int determineKeyValIndex(const CommandDescription &cd)
+{
+    int cnt=0;
+    for(int i=0;i<cd.arguments.length();++i){
+        if(cd.arguments.at(i).tokenType==Token::keyValArg){
+            ++cnt;
+        }
+    }
+    return cnt;
 }
 
 
