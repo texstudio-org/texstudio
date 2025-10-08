d9416c6a7c56d97e6c34e1142a715ad8b46147e1
Jan Sundermeyer <sunderme@gmx.de>
2025-10-08 21:01:48 +0200

    fix completer word detection on empty line

--- src/latexcompleter.cpp
+++ src/latexcompleter.cpp
@@ -1910,7 +1910,7 @@ void LatexCompleter::complete(QEditor *newEditor, const CompletionFlags &flags)
         if(i<0){
             start=0; // take complete text if no eow is detected (#3966)
             // skip spaces at the beginning
-            i = c.columnNumber() - 1;
+            i = c.columnNumber();
             while(start<lineText.length() && lineText.at(start)==' ' && start<i){
                 ++start;
             }
